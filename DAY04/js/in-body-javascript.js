ee2c92e5108eaaf37c90889defb472c9bac11d34
Jee Hoon <yamoo9@naver.com>
2016-06-21 17:25:16 +0900

    script, noscript 사용 법

---
+++ DAY04/js/in-body-javascript.js
@@ -0,0 +1,6 @@
+document.write('현재 사용자의 환경은 온라인입니까? ', window.navigator.onLine ? '네 온라인입니다.' : '아니오. 오프라인입니다.');
+
+
+setTimeout(function() {
+  document.write('<h1>This is JavaScript!</h1>');
+}, 3000);
