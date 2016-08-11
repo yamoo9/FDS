6f658ab0802165ac6c7bcb25bf17e4c3724d8e23
Jee Hoon <yamoo9@naver.com>
2016-08-11 16:23:08 +0900

    크로스 브라우징 getStyle() 헬퍼 함수 추가

--- Lecture/DAY32/js/control-text-size.js
+++ Lecture/DAY32/js/control-text-size.js
@@ -35,8 +35,53 @@ function changeTextSize() {
 // console.log('decrease_btn:', decrease_btn);
 
 // page_header 요소 객체의 글자 크기 초기화
-page_header.style.fontSize = '16px';
+// page_header.style.fontSize = '16px';
 
 // 버튼에 이벤트 핸들링(핸들러 - 바인딩)
 increase_btn.onclick = changeTextSize;
 decrease_btn.onclick = changeTextSize;
+
+
+// ------------------------------------------------
+// 웹 브라우저에서 계산된 CSS 스타일 값 가져오는 방법
+// ------------------------------------------------
+// 비 표준 MS IE 방식 (IE 8-)
+// 대상.currentStyle.스타일속성
+// ------------------------------------------------
+// 표준 W3C 방식 (IE 9+)
+// window.getComputedStyle(대상,가상요소).스타일속성
+// ------------------------------------------------
+function getStyle(el, property, pseudo) {
+  var value;
+  // 유효성 검사
+  if ( el.nodeType !== 1 ) {
+    console.error('첫번째 인자 el은 요소노드여야 합니다.');
+  }
+  if ( typeof property !== 'string' ) {
+    console.error('두번째 인자 property는 문자열이야 합니다.');
+  }
+  if ( typeof pseudo !== 'string' && pseudo ) {
+    console.error('세번째 인자 pseudo는 문자열이야 합니다.');
+  }
+  if (window.getComputedStyle) {
+    value = window.getComputedStyle(el,pseudo)[property];
+  } else {
+    value = el.currentStyle[property];
+  }
+  return value;
+}
+
+// ------------------------------------------------
+// 대괄호 표기법
+// ------------------------------------------------
+// getStyle(el, 'outline')
+
+// 펜.색상
+// 펜.길이
+// 펜.용도
+// 펜.가격
+// 펜.종료
+
+// 속성 = '색상';
+
+// 펜[속성]
