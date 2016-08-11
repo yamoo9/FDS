f51d5043f3bd5dba0ac4e379a857afc101164005
Jee Hoon <yamoo9@naver.com>
2016-08-11 14:43:14 +0900

    텍스트 크기 함수 분리 제어

--- Lecture/DAY32/js/control-text-size.js
+++ Lecture/DAY32/js/control-text-size.js
@@ -2,6 +2,23 @@
 var page_header  = query('.page-header');
 var increase_btn = query('.btn-increase-text');
 var decrease_btn = query('.btn-decrease-text');
+var change_text  = 10;
+
+// 글자 크기 변경 함수
+function changeTextSize() {
+  // console.log('context:', this.firstChild.nodeValue);
+  var operator = this.firstChild.nodeValue;
+  var _change_text = change_text;
+  // page_header의 글자 크기를 키운다.
+  // 글자 크기를 연산이 가능하도록 변경
+  var current_size = parseInt(page_header.style.fontSize);
+  // 글자 크기를 변경(키움)하여 해당 요소에 설정
+  if (operator === '-') {
+    _change_text = change_text * -1;
+  }
+  page_header.style.fontSize = current_size + _change_text + 'px';
+  console.log(page_header.style.fontSize);
+};
 
 // 검증
 // console.log('page_header:', page_header);
@@ -12,11 +29,5 @@ var decrease_btn = query('.btn-decrease-text');
 page_header.style.fontSize = '16px';
 
 // 버튼에 이벤트 핸들링(핸들러 - 바인딩)
-increase_btn.onclick = function() {
-  // page_header의 글자 크기를 키운다.
-  console.log(page_header.style.fontSize);
-};
-
-decrease_btn.onclick = function() {
-  // page_header의 글자 크기를 줄인다.
-};
+increase_btn.onclick = changeTextSize;
+decrease_btn.onclick = changeTextSize;
