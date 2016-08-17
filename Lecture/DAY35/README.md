fa69bd2e8dc7d44bb5f13ff93150739df6b66295
Jee Hoon <yamoo9@naver.com>
2016-08-17 14:38:09 +0900

    콜백 함수 패턴에 대한 실습 설명

--- Lecture/DAY35/README.md
+++ Lecture/DAY35/README.md
@@ -1,3 +1,39 @@
 ###### Front-End Develop SCHOOL
 
 # DAY 35
+
+offsetX 이벤트 대상 객체에서의 상대적 마우스 X좌표 위치를 반환 - 지역좌표 (IE만 지원)
+offsetY 이벤트 대상 객체에서의 상대적 마우스 Y좌표 위치를 반환 - 지역좌표 (IE만 지원)
+
+layerX 이벤트 대상 객체에서의 상대적 마우스 Y좌표 위치를 반환 - 지역좌표 (IE 지원 안함)
+layerY 이벤트 대상 객체에서의 상대적 마우스 Y좌표 위치를 반환 - 지역좌표 (IE 지원 안함)
+clientX 브라우저 페이지에서의 X좌표 위치를 반환하나 스크롤 무시하고 해당 페이지의 상단을 0 으로 잡음 - 글로벌 좌표
+clientY 브라우저 페이지에서의 Y좌표 위치를 반환하나 스크롤 무시하고 해당 페이지의 상단을 0 으로 잡음 - 글로벌 좌표
+
+pageX 브라우저 페이지에서의 X좌표 위치를 반환(스크롤 반영함) - 글로벌좌표(IE 지원 안함)
+pageY 브라우저 페이지에서의 Y좌표 위치를 반환(스크롤 반영함) - 글로벌좌표(IE 지원 안함)
+
+screenX 전체 모니터 스크린에서의 마우스 X좌표 위치를 반환
+screenY 전체 모니터 스크린에서의 마우스 Y좌표 위치를 반환
+
+
+IE 에서 pageX, pageY 생성하는 법. (실제로 jQuery 에서 다음처럼 구합니다.)
+
+function fixEvent(event) {
+  if ( !event.target ) {
+    event.target = event.srcElement || document;
+  }
+
+  if ( event.target.nodeType === 3 ) {
+    event.target = event.target.parentNode;
+  }
+
+  if ( event.pageX == null && event.clientX != null ) {
+    var eventDocument = event.target.ownerDocument || document,
+    doc = eventDocument.documentElement,
+    body = eventDocument.body;
+
+    event.pageX = event.clientX + (doc && doc.scrollLeft || body && body.scrollLeft || 0) - (doc && doc.clientLeft || body && body.clientLeft || 0);
+    event.pageY = event.clientY + (doc && doc.scrollTop  || body && body.scrollTop  || 0) - (doc && doc.clientTop  || body && body.clientTop  || 0);
+  }
+}
--- Lecture/DAY35/js-anim.js
+++ Lecture/DAY35/js-anim.js
@@ -11,21 +11,57 @@
 // .demo-box 요소를 선택
 var demo_box = query('.demo-box');
 
-function movingBox(position, callback) {
+function movingBox(box, position, callback) {
+  // 데이터 검증
+  if ( isntElNode(box) ) {
+    errorMsg('요소 노드여야만 합니다.');
+  }
   // 초기 값 설정
   position = position || { 'x': 0, 'y': 0 };
-  callback = typeof callback === 'function' ? callback : function() {};
+  // callback = typeof callback === 'function' ? callback : function() {};
+  console.log(position);
 
-  // 애니메이션 시동!!!!!
+  // console.dir(box);
+  var current = {
+    'x': box.offsetLeft,
+    'y': box.offsetTop
+  };
 
-  // 애니메이션 종료!!!!
-  callback();
+  // 목표 점
+  // console.log('distance x', position.x - current.x);
+  // console.log('distance y', position.y - current.y);
+
+  var target_x = current.x + 100;
+
+  var interval_id = setInterval(function() {
+    if (current.x <= target_x) {
+      current.x += 10;
+      box.style.left = current.x + 'px';
+    } else {
+      clearInterval(interval_id);
+      // 애니메이션 종료!!!!
+      if (callback && typeof callback  === 'function') {
+        callback();
+      }
+    }
+    // box.style.top = current.y + 10 + 'px';
+  }, 200);
 }
 
+// ---------------------------------------------------------------------------------------
+// 스크롤과 상관 없이 브라우저 뷰포트의 좌측 상단에서의 위치
+// ---------------------------------------------------------------------------------------
+// MouseEvent.clientX은 event가 발생한 지점의 클라이언트에서의 X 좌표를 참조 할 수있는 읽기 전용 속성입니다.
+// 화면의 왼쪽에서 클릭하면 페이지의 가로 스크롤 위치에 관계없이 그 값은 0입니다.
+// MouseEvent.clientX
+// MouseEvent.clientY
+
 demo_box.onclick = function(event) {
-  console.log('clicked demo box.');
-  movingBox({
+  // console.log('clicked demo box.');
+  movingBox(this, {
     'x': event.clientX,
     'y': event.clientY
+  }, function() {
+    alert('애니메이션 끝났다구!!!');
   });
 };
