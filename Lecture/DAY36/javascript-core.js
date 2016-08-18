b26521993d12f5f7ca25cf54be63f8ce9e50540a
Jee Hoon <yamoo9@naver.com>
2016-08-18 16:54:40 +0900

    내장 객체에 한해서는 리터럴 표현식을 사용하는 것을 권장한다.

--- Lecture/DAY36/javascript-core.js
+++ Lecture/DAY36/javascript-core.js
@@ -84,3 +84,50 @@
   // 각 버튼을 사용자가 클릭할 경우, 자신의 순환 인덱스 숫자를 콘솔에 출력한다.
 
 })(this);
+
+
+
+(function(global){
+  'use strict';
+  // 생성자 역할 수행하는 함수
+  // 이름 짓는 관례 상 첫글자가 대문자로 작성된다.
+  function Navigation(name) {
+      this.name = name;
+  //  return 객체를 생성해서 반환한다.
+  }
+
+  var gnb = new Navigation('글로벌 내비게이션 바');
+  var lnb = new Navigation('로컬 내비게이션 바');
+  var unb = new Navigation('유틸리티 내비게이션 바');
+})(this);
+
+(function(global){
+  'use strict';
+
+  // 숫자 객체 생성자를 사용하여 숫자 1부터 10까지를 변수 num1 ~ num10에 생성하여라.
+  // 단, num1 ~ num10에는 숫자 값이 담겨야 한다.
+  global.num1  = (new Number(1)).valueOf();
+  global.num2  = (new Number(2)).valueOf();
+  global.num3  = (new Number(3)).valueOf();
+  global.num4  = (new Number(4)).valueOf();
+  global.num5  = (new Number(5)).valueOf();
+  global.num6  = (new Number(6)).valueOf();
+  global.num7  = (new Number(7)).valueOf();
+  global.num8  = (new Number(8)).valueOf();
+  global.num9  = (new Number(9)).valueOf();
+  global.num10 = (new Number(10)).valueOf();
+
+  // 불리언 객체 생성자를 사용하여 불리언 true, false로 연속되는 변수 boo1 ~ boo10에 생성하여라.
+  // 단, boo1 ~ boo10에는 불리언 값이 담겨야 한다.
+  global.boo1  = (new Boolean(true)).valueOf();
+  global.boo2  = (new Boolean(false)).valueOf();
+  global.boo3  = (new Boolean(true)).valueOf();
+  global.boo4  = (new Boolean(false)).valueOf();
+  global.boo5  = (new Boolean(true)).valueOf();
+  global.boo6  = (new Boolean(false)).valueOf();
+  global.boo7  = (new Boolean(true)).valueOf();
+  global.boo8  = (new Boolean(false)).valueOf();
+  global.boo9  = (new Boolean(true)).valueOf();
+  global.boo10 = (new Boolean(false)).valueOf();
+
+})(this);
