236eb6f606a9d4aba03b76ce8fa6f6490a9a521f
Jee Hoon <yamoo9@naver.com>
2016-08-23 13:50:23 +0900

    글자를 <span>으로 감싸도록 구성

--- Lecture/DAY39/js/jquery.study.js
+++ Lecture/DAY39/js/jquery.study.js
@@ -51,4 +51,34 @@
   console.log('#1 - width', $body.css('width'));
   console.log('#2 - height', $body.css('height'));
 
+});
+// (this, this.jQuery)
+
+
+
+// <h1> 요소 내부의 텍스트 글자를 쪼개서
+// 각각의 글자를 <span> 요소로 감싸도록 구성한다.
+// 각각의 <span> 요소에 마우스 엔터 이벤트를 설정하여
+// 글자의 색상을 변경한다.
+(function(global, $){
+  'use strict';
+
+  var $h1 = $('.container h1');
+  var h1_text = $h1.text();
+  // console.log(h1_text); // 'jQuery Method'
+  // 문자 -> 배열화
+  h1_text = h1_text.split('');
+  // console.log(h1_text); // ['j','Q',...,'d']
+  // jQuery.map() 유틸리티 메소드(Static Method, Class Method)
+  h1_text = $.map(h1_text, function(item, index) {
+    if($.trim(item)) {
+      // console.log('<span>'+item+'</span>');
+      return '<span>'+item+'</span>';
+    } else {
+      return item;
+    }
+  });
+  h1_text = h1_text.join('');
+  $h1.html(h1_text);
+
 })(this, this.jQuery);
