f8cf4c7d56652add967596fdc82bd70d28c010a8
Jee Hoon <yamoo9@naver.com>
2016-08-25 13:28:16 +0900

    비동기 통신 객체에 이벤트 핸들러 바인딩

--- Lecture/DAY41/js/ajax-communication.js
+++ Lecture/DAY41/js/ajax-communication.js
@@ -43,19 +43,31 @@
   //   console.log(xhr);
   // }
 
-  // xhr.open('GET', 'data/data.txt', false); // 성공
-  xhr.open('GET', 'data/model.txt', false); // 실패
+  // xhr.open('GET', 'data/data.txt', true); // 비동기 통신
+  xhr.open('GET', 'data/data.txt'); // 비동기 통신
+  // xhr.open('GET', 'data/model.txt', false); // 동기 통신
 
-  function updateViewPlace() {
-    xhr.send();
-    if ( xhr.status === 200 && xhr.readyState === 4 ) {
+  // 비동기 통신 객체에 이벤트 핸들러 바인딩
+  xhr.onreadystatechange = function() {
+    console.log(this); // this === xhr 객체
+    if ( this.status === 200 && this.readyState === 4 ) {
       console.log('통신 데이터 전송 성공! ^ㄴ^');
-      result_view.textContent = '[' + xhr.statusText + '] ' + xhr.response;
+      result_view.textContent = '[' + this.statusText + '] ' + this.responseText;
     } else {
       console.log('통신 데이터 전송 실패! ㅠ_ㅠ');
-      result_view.textContent = '[' + xhr.statusText + '] ' + '데이터 로드에 실패했습니다....';
+      result_view.textContent = '[' + this.statusText + '] ' + '데이터 로드에 실패했습니다....';
     }
     console.log(xhr);
   }
+
+  function updateViewPlace() {
+    // AJAX 통신 요청 보내기
+    xhr.send();
+    // 비동기 통신을 요청했을 경우,
+    // 이벤트(`readystatechange`)를 통해 비동기 데이터가 로드된 시점에
+    // 아래 조건문이 수행되어야 한다.
+  }
+
+
 
 })(this, this.XMLHttpRequest);
