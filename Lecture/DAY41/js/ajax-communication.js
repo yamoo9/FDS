203d171b9ccbb3fff3e72b0287be7666d78e903d
Jee Hoon <yamoo9@naver.com>
2016-08-25 12:03:39 +0900

    동기 통신의 경우

--- Lecture/DAY41/js/ajax-communication.js
+++ Lecture/DAY41/js/ajax-communication.js
@@ -18,8 +18,8 @@
   // 2. OPEN
   // 마지막 인자 값이 false 라면 동기 통신!
   // 마지막 인자 값이 true 또는 생략하면 비동기 통신!
-  xhr.open('GET', 'data/data.txt', false); // 동기 통신 Deprecated!!
-  // xhr.open('GET', 'data/data.txt'); // 비동기 통신
+  // xhr.open('GET', 'data/data.txt', false); // 동기 통신 Deprecated!!
+  xhr.open('GET', 'data/data.txt'); // 비동기 통신
 
   // 3. SEND
   xhr.send();
@@ -27,13 +27,17 @@
   // 동기 통신일 경우는.... 데이터가 로드될 때까지 기다림...
   // 비동기 통신일 경우는.... 아래 코드가 바로 해석됨...
 
+  var result_view = document.querySelector('.ajax-result');
+
   // 통신 상태 확인
   if ( xhr.status === 200 ) {
     console.log('통신 데이터 전송 성공! ^ㄴ^');
     // console.log(xhr.response);
-    document.querySelector('.ajax-result').textContent = xhr.response;
+    result_view.textContent = xhr.response;
   } else {
     console.log('통신 데이터 전송 실패! ㅠ_ㅠ');
+    result_view.textContent = '데이터 로드에 실패했습니다....';
+    console.log(xhr);
   }
 
 })(this, this.XMLHttpRequest);
