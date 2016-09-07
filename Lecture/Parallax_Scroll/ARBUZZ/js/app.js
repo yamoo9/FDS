ec528a166b991e645b1592823726437e5945338a
Jee Hoon <yamoo9@naver.com>
2016-09-07 19:19:16 +0900

    포트폴리오 상담 내용 추가

--- Lecture/Parallax_Scroll/ARBUZZ/js/app.js
+++ Lecture/Parallax_Scroll/ARBUZZ/js/app.js
@@ -7,55 +7,6 @@
 
   global.Detectizr.detect();
 
-  /////////////////
-  // ScrollMagic //
-  /////////////////
-
-  // Controller 객체 설정
-  var ctrl = new SM.Controller();
-
-  ////////////////////////////////
-  // 패럴럭스 씬 컨트롤(제어) 반복 구문 //
-  ////////////////////////////////
-
-  var scene_list = '.banner-container, .products-tab, .about-us, .from-the-blog, .footer > *'.split(', ');
-
-  scene_list.forEach(function(trigger_el_selector, idx) {
-    var scene = new SM.Scene({
-      'triggerElement': trigger_el_selector,
-      'triggerHook': 0,
-      'offset': -600,
-      // 'duration': 100,
-      // 'reverse': false
-    })
-    .setClassToggle(trigger_el_selector, 'fade-in')
-    .addTo(ctrl);
-
-    switch ( trigger_el_selector ) {
-      case '.banner-container':
-        scene.setTween( banner_tl );
-      break;
-      case '.products-tab':
-        scene.setTween( products_tl );
-      break;
-    }
-
-  });
-
-  //////////////////
-  // 핀 컨트롤(제어) //
-  //////////////////
-
-  var carousel_pin = new SM.Scene({
-    'triggerElement': '.carousel',
-    'triggerHook': 0,
-    'duration': 200
-  });
-
-  carousel_pin
-    .setPin('.carousel', {'pushFollowers': false})
-    .setTween(carousel_tl)
-    .addTo(ctrl);
 
   ///////////////////////////////////
   // GreenSock TimelineMax 애니메이션 //
@@ -89,6 +40,16 @@
       'x': -1000,
       'ease': Quad.easeOutBack
     } )
+    .from('.banner-shoes h3', 0.6, {
+      'autoAlpha': 0,
+      'y': 500,
+      'ease': Quad.easeOutBack
+    }, 0.8 )
+    .from('.banner-shoes span', 1, {
+      'autoAlpha': 0,
+      'y': 20,
+      'ease': Quad.easeInOut
+    }, 1.2 )
     .from('.banner-leather-bags', 0.6, {
       'autoAlpha': 1,
       'x': 1000,
@@ -126,6 +87,15 @@
       'autoAlpha': 1,
       'scale': 1
     }, 0.2)
+    .fromTo('.products-tab .product-item:nth-child(1) img', 0.3,
+    {
+      'autoAlpha': 0,
+      'scale': 0
+    },
+    {
+      'autoAlpha': 1,
+      'scale': 1
+    }, 0.45)
     .fromTo('.products-tab .product-item:nth-child(2)', 0.4,
     {
       'autoAlpha': 0,
@@ -153,5 +123,74 @@
       'autoAlpha': 1,
       'scale': 1
     }, 0.8);
+
+  // 어바웃
+  var about_tl = new TimelineMax();
+  about_tl
+    .fromTo('.about-us .headline', 1, {
+      'y': -10,
+      'scale': 0,
+      'rotation': 360,
+      'autoAlpha': 0
+    },
+    {
+      'y': 0,
+      'scale': 1,
+      'rotation': 0,
+      'autoAlpha': 1
+    }, 0.9);
+
+  /////////////////
+  // ScrollMagic //
+  /////////////////
+
+  // Controller 객체 설정
+  var ctrl = new SM.Controller();
+
+  ////////////////////////////////
+  // 패럴럭스 씬 컨트롤(제어) 반복 구문 //
+  ////////////////////////////////
+
+  var scene_list = '.banner-container, .products-tab, .about-us, .from-the-blog, .footer > *'.split(', ');
+
+  scene_list.forEach(function(trigger_el_selector, idx) {
+    var scene = new SM.Scene({
+      'triggerElement': trigger_el_selector,
+      'triggerHook': 0,
+      'offset': -600,
+      // 'duration': 100,
+      // 'reverse': false
+    })
+    .setClassToggle(trigger_el_selector, 'fade-in')
+    .addTo(ctrl);
+
+    switch ( trigger_el_selector ) {
+      case '.banner-container':
+        scene.setTween( banner_tl );
+      break;
+      case '.products-tab':
+        scene.setTween( products_tl );
+      break;
+      case '.about-us':
+        // scene.setTween( about_tl );
+      break;
+    }
+
+  });
+
+  //////////////////
+  // 핀 컨트롤(제어) //
+  //////////////////
+
+  var carousel_pin = new SM.Scene({
+    'triggerElement': '.carousel',
+    'triggerHook': 0,
+    'duration': 200
+  });
+
+  carousel_pin
+    .setPin('.carousel', {'pushFollowers': false})
+    .setTween(carousel_tl)
+    .addTo(ctrl);
 
 })(this, this.jQuery, this.ScrollMagic);
--- Lecture/Portfolio/c.md
+++ Lecture/Portfolio/c.md
@@ -29,3 +29,124 @@
 - [과제 1] PSD -> HTML/CSS [ Indurge (로고 변경, 브랜드 색상, 이미지 교체, 텍스트 한글), YG Music 비주얼 리뉴얼 ]
 - [과제 2] Arbuzz UI Kit → HTML/CSS/JS
 - [과제 3] Chatting UI Visual 구현 + 로컬 스토리지
+
+-
+
+## 윤상
+
+- Amazon 사이트 UI 구현
+- 북미터(일본) 사이트 서비스 개선
+- UX/UI 비교 정리
+
+-
+
+## 영선
+
+[채용] - 웹 퍼블리싱, 스타트업 / 에이전시 / 인력 아웃소싱
+
+유니콘 영선
+
+------------------------------------------------
+Design [ Layout / Plan ]
+HTML
+CSS
+JS <<<<<<
+------------------------------------------------
+
+- 동부화재 [ 메인 / 자동차 보험 / 고객센터 / 법인 서비스 / 보상 서비스 / 오토케어 서비스 ] 6 페이지
+- FDS 과제 정리
+- 디자인 결과물 정리
+- github.io 계정 업로드
+
+-
+
+## 한재현
+
+- PHP / Database + HTML5/CSS3/JS/jQuery/Angular
+- Airbnb + Parallax [1~2 Pages]
+- Carol 웹사이트
+- FAST SHOP 웹사이트 [3 Pages]
+- 스터디 진행 내용을 정리한 레퍼런스 사이트
+- github.io 업데이트
+
+-
+
+## 기석
+
+- 3D 캐러셀 Vanilla JavaScript 라이브러리 제작 (IE 10+)
+- ScrollMagic + GreenSock 라이브러리 활용
+- 도큐멘테이션 페이지
+- github.io 등록 / 로그
+- 기존 과제 정리 ( + Aware Sub Page )
+
+-
+
+## 정식
+
+- BeRealist 서비스 구현 (AngularJS + Firebase)
+- FDS.USER.API 서비스
+- eatdesignlove UI Components 제작 (Carousel, Tab, Navigation)
+- github.io 정리
+
+-
+
+## 장재현
+
+경력 6년 이상의 Front-End 서비스 구현 기술을 잘 알고 있는 동안 기획자
+UX/UI에 관련된 내용을 정리하여 기술한 뷰를 제공하는 페이지
+- 배운 내용을 정리하는 시간을 가진다.
+- 신생 블로그 생성 후에 내용을 정리하는 페이지를 구현
+- SNS에 공유
+
+-
+
+## 지연
+
+- Dashboard ( SVG Path Animation + AngularJS + Ajax = Data View) 제공
+- 데이터를 뿌린 후의 뷰를 HTML/CSS/JS로 구현
+- 데이터 준비...
+- 과제 ( 피지리아 + 이미지 변경 + 패럴럭스 적용 )
+- github.io 등록
+
+-
+
+## 예연
+
+스타트업 (꿈/야망)
+
+- 날씨 앱
+- InVision, Principle 활용
+- 날씨 이펙트 SVG 애니메이션 활용
+- 디자인 프로세스 로그 [ 와이어프레임 -> 프로토타입 -> 비주얼 목업 ]
+
+-
+
+## 호민
+
+- 모멘텀 + Open API
+- Google 확장 프로그램(Extension)에 등록
+- Life of PI 패럴럭스 사이트 오마쥬 구현
+
+-
+
+## 병은
+
+- 기성 사이트 1개 사이트 선정 > 웹 접근성 개선 프로젝트
+- 과제 보완/완성 (★★★★★)
+- DC (Batman) 캐릭터 JSON을 활용한 SPA << Angular + jQuery / Greensock + ScrollMagic Library
+
+-
+
+## 현정
+
+사수가 있는 기업
+
+- UI Kit 스타일 가이드 사이트 구현 (Sass)
+- 과제 정리 -> 코드 리팩토링 + 패럴럭스 애니메이션 효과 적용
+- github.io 페이지 등록
+
+-
+
+## 효선
+
+- 모바일 앱 View 페이지 구현 + GreenSock 모션 적용 (`<iframe>` 사용)
