e538555cf559cbec65a01a0df0c083d95e07894e
Jee Hoon <yamoo9@naver.com>
2016-10-12 14:20:32 +0900

    toggleGrid 모듈 추가

--- 2nd/Modules/JS/gs-utils.js
+++ 2nd/Modules/JS/gs-utils.js
@@ -4,3 +4,18 @@
 function percent(target, context) {
   return target/context * 100;
 }
+
+/** @function toggleGrid() */
+function toggleGrid() {
+  var _container = document.querySelector('.container');
+  // 조건 검증
+  // _container 요소에 .show-grid 클래스가 있어?
+  // 있으면?
+  if ( _container.classList.contains('show-grid')  ) {
+    _container.classList.remove('show-grid');
+  }
+  // 없으면?
+  else {
+    _container.classList.add('show-grid');
+  }
+}
