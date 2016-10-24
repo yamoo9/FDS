1efffd8afd3e48ba2ef3a3104471a2e11568132d
Jee Hoon <yamoo9@naver.com>
2016-10-24 19:24:21 +0900

    Simple Photo Gallery 수정

--- 2nd/DAY17/simple-photo-gallery/js/simple-photo-gallery.js
+++ 2nd/DAY17/simple-photo-gallery/js/simple-photo-gallery.js
@@ -2,21 +2,20 @@
 
 // TODO:
 // HTML Structure
-// Sass Presentation}
-
+// Sass Presentation
 // JavaScript Behavior
 
 //  1. `.simple-photo-gallery` 요소에 접근
-var gallery = document.querySelector('.simple-photo-gallery');
+var gallery                 = document.querySelector('.simple-photo-gallery');
 var gallery_control_buttons = gallery.querySelectorAll('button');
-var gallery_view = gallery.querySelector('.gallery-view');
+var gallery_view            = gallery.querySelector('.gallery-view');
 
 var chageGalleryView = function(){
-  var img = this.querySelector('img');
-  var view = gallery_view.querySelector('img');
-  // var src = img.src; // HTML DOM
-  var src = img.getAttribute('src'); // XML DOM
-  var alt = img.getAttribute('alt'); // XML DOM
+  var img         = this.querySelector('img');
+  var view        = gallery_view.querySelector('img');
+  // var src      = img.src; // HTML DOM
+  var src         = img.getAttribute('src'); // XML DOM
+  var alt         = img.getAttribute('alt'); // XML DOM
   var changed_src = src.replace('thumbs', 'big');
   // 설정
   view.setAttribute('src', changed_src);
@@ -24,5 +23,5 @@ var chageGalleryView = function(){
 };
 
 for (var i=0, l=gallery_control_buttons.length; i<l; i++) {
-  gallery_control_buttons.item(i).onclick = chageGalleryView;
+  gallery_control_buttons[i].onclick = chageGalleryView;
 }
