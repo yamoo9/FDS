65ded2466c4b9921272ec0874d2daa46c65ce5eb
Jee Hoon <yamoo9@naver.com>
2016-10-25 14:00:05 +0900

    자바스크립트 함수 객체 파일 분리

---
+++ 2nd/DAY18/function.js
@@ -0,0 +1,17 @@
+////////////////////
+// 자바스크립트 함수  //
+////////////////////
+
+// 함수 선언식
+function fnDeclaration() {
+  console.log('함수 선언문을 실행했습니다.');
+}
+
+// 함수 표현식
+var fnExpression = function(){
+  console.log('함수 표현식을 참조한 변수를 실행했습니다.');
+};
+
+// 함수 실행
+fnDeclaration();
+fnExpression();
--- 2nd/DAY18/review.js
+++ 2nd/DAY18/review.js
@@ -77,15 +77,3 @@ for (var p in kijoong) {
 
 // ES 6 (ECMA Script 2015)
 // for ( var [key, value] of [Array, Object, Map] ) { ... }
-
-
-
-////////////////////
-// 자바스크립트 함수  //
-////////////////////
-
-// 함수 선언식
-function fnDeclaration() { console.log('함수 선언문을 실행했습니다.'); }
-
-// 함수 표현식
-var fnExpression = function(){ console.log('함수 표현식을 참조한 변수를 실행했습니다.'); };
