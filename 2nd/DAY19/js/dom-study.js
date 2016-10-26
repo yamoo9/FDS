da5e5b4e7bce2188d552214ec9061e26bc922c57
Jee Hoon <yamoo9@naver.com>
2016-10-26 18:05:02 +0900

    클로저 사용 예

--- 2nd/DAY19/js/dom-study.js
+++ 2nd/DAY19/js/dom-study.js
@@ -11,9 +11,26 @@
   var doc        = global.document;
   var gnb        = doc.querySelector('#gnb'); // ElementNode
   var gnb_links  = gnb.querySelectorAll('a'); // NodeList
-  var print_area = doc.querySelector('#print_area');
-  var updatePrintMessage = function () {
-    // ????
+  var print_area = doc.querySelector('#print-area');
+  var i; // undefined
+  var l;
+
+  var wrapperUpdatePrintMessage = function(i) {
+    var _updatePrintMessage = function () {
+      // 함수 내부의 this 키워드는 함수를 실행한 주체를 가리킨다.
+      // console.log(this);
+      var message = this.getAttribute('data-print-message');
+      // #print-area
+      // console.log(print_area.firstChild.nodeValue);
+      // GET
+      // console.log(print_area.innerHTML);
+      // SET
+      print_area.innerHTML = message;
+      console.log(i); // 5
+      // 웹 브라우저의 기본 동작을 차단
+      return false;
+    };
+    return _updatePrintMessage;
   }
 
   // NodeList <= Like Array Object
@@ -21,12 +38,29 @@
 
   // length 속성이 있는 객체를 순환할 준비가 되었다.
   // 반복 구문 사용할 수 있다.
-  for ( var i=0, l=gnb_links.length; i<l; i++ ) {
-    var gnb_link = gnb_links[i];
+  i = 0;
+  l = gnb_links.length;
+  for ( ; i<l; i++ ) {
+    var gnb_link = gnb_links[i]; // 0, 1, 2, 3, 4
     // gnb_link // NodeList ?, ElementNode ?
     // 문서객체 집합을 순환한 이유: 이벤트 핸들러(함수) 연결
-    gnb_link.onclick = updatePrintMessage;
+    gnb_link.onclick = wrapperUpdatePrintMessage(i);
+    // gnb_link.onclick = function () {
+    //   // 함수 내부의 this 키워드는 함수를 실행한 주체를 가리킨다.
+    //   // console.log(this);
+    //   var message = this.getAttribute('data-print-message');
+    //   // #print-area
+    //   // console.log(print_area.firstChild.nodeValue);
+    //   // GET
+    //   // console.log(print_area.innerHTML);
+    //   // SET
+    //   print_area.innerHTML = message;
+    //   console.log(i); // 5
+    //   // 웹 브라우저의 기본 동작을 차단
+    //   return false;
+    // };
   }
-
+  // i = 5
+  // global.gnb_links = gnb_links;
 
 })(this);
