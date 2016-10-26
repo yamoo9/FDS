93c2b1ad2c821816ad372ba310ac31de9a88cf44
Jee Hoon <yamoo9@naver.com>
2016-10-26 17:36:20 +0900

    문서 객체 제어를 위한 구문 추가

--- 2nd/DAY19/js/dom-study.js
+++ 2nd/DAY19/js/dom-study.js
@@ -5,8 +5,28 @@
 (function(global){
   'use strict';
 
-  // console.log('global === window:', global === window);
+  // 문서에서 제어할 객체를 각 변수에 참조합니다.
+  // [ IE 8+ ] .querySelector(), .querySelectorAll()
+  // #gnb, #print-area
+  var doc        = global.document;
+  var gnb        = doc.querySelector('#gnb'); // ElementNode
+  var gnb_links  = gnb.querySelectorAll('a'); // NodeList
+  var print_area = doc.querySelector('#print_area');
+  var updatePrintMessage = function () {
+    // ????
+  }
+
+  // NodeList <= Like Array Object
+  console.log( 'gnb_links.length:', gnb_links.length );
+
+  // length 속성이 있는 객체를 순환할 준비가 되었다.
+  // 반복 구문 사용할 수 있다.
+  for ( var i=0, l=gnb_links.length; i<l; i++ ) {
+    var gnb_link = gnb_links[i];
+    // gnb_link // NodeList ?, ElementNode ?
+    // 문서객체 집합을 순환한 이유: 이벤트 핸들러(함수) 연결
+    gnb_link.onclick = updatePrintMessage;
+  }
+
 
 })(this);
-
-// console.log(global); // 오류 발생
