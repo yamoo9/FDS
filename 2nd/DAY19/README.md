c08886ced1c3b041f6a56257dac1ca7784372716
Jee Hoon <yamoo9@naver.com>
2016-10-26 13:03:00 +0900

    NVM 설치 방법 추가

---
+++ 2nd/DAY19/README.md
@@ -0,0 +1,73 @@
+###### Front-End Develop SCHOOL
+
+# DAY 19
+
+---
+
+# NVM(Node Version Manager) 설치
+
+## Windows 설치
+
+[nvm-windows](https://github.com/coreybutler/nvm-windows/releases)
+
+## OSX 설치
+
+### 1. homebrew를 사용해서 인스톨 한 node.js 모두 삭제
+
+[이슈](https://github.com/creationix/nvm/issues/855#issuecomment-146115434)에서 볼 수 있듯, `v0.31.1`의 `nvm`은 `osx`의 `homebrew`를 지원하지 않음. `brew`를 통해서 `nvm` 및 `node`를 설치하면 충돌이 발생! `nvm`을 사용하시기로 마음 먹었다면, `homebrew`를 사용해서 인스톨 한 `nvm`, `node.js`를 모두 삭제.
+
+```sh
+$ sudo brew uninstall node —force sudo brew uninstall nvm —force
+```
+
+### 2. nvm 설치
+
+[NVM:GitHub](https://github.com/creationix/nvm)의 지시에 따라서 `nvm`을 설치.
+
+최신 버젼은 아래의 커맨드로 설치.
+
+```sh
+$ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh | bash
+```
+
+### 3. nvm 실행
+
+`nvm` 자동 실행시키기. 이제부터 실행시키는 모든 `node`의 버젼은 `nvm`이 관리하게 될 것.
+하지만 위 작업은 영구적인 것이 아니기 때문에, 터미널이 오픈될 때마다 실행시켜주어야 한다는 불편함이 있음.
+따라서 사용 중인 컴퓨터의 `.profile`, `.bashrc` 혹은 `.zshrc`에 다음의 문구를 추가.
+
+```sh
+$ source /Users/$USER/.nvm/nvm.sh
+```
+
+터미널이 오픈될 때마다, `nvm`이 골라준 `node`를 실행시킬 수 있음.
+
+#### nvm, 설치 가능한 Node 버전 탐색
+
+```sh
+$ nvm ls-remote
+```
+
+#### nvm, 새로운 Node 버전 설치
+
+```sh
+$ nvm install #{version} #6.6.0
+```
+
+#### nvm, 설치된 Node 버전 보기
+
+```sh
+$ nvm ls
+```
+
+#### nvm, 사용할 Node 버전 설정
+
+```sh
+nvm use #{version}
+```
+
+### NPM 최신 버전 업데이트
+
+```js
+$ npm install -g npm@latest
+```
--- 2nd/DAY19/function-2nd.js
+++ 2nd/DAY19/function-2nd.js
@@ -3,36 +3,60 @@
 // 전역
 // 느슨한 모드
 
-// 자바스크립트 함수를 `Frist Class Object`라고 부르는 이유 중 하나.
+// 자바스크립트 함수를 일급객체(`Frist Class Object`)라고 부르는 이유 중 하나.
+// 함수의 인자로 함수를 전달할 수 있다. [콜백 패턴]
 function localScope(callback) {
   // 엄격 모드 실행
   'use strict';
   // 특정 코드를 수행 ...
+  for ( var i=0, l=100; i<l; i+=4 ) {
+    console.log('i:', i);
+  }
   // 전달 받은 인자(함수)를 실행
   // 조건 구문을 사용하여 올바른 데이터 유형을 감지한 후, 올바르다면 코드 실행
   // 전달 받은 인자의 유형이 함수일 경우에만 callback() 실행
 
   // 1. if문을 사용할 경우
   var is_callback = typeof callback === 'function';
-  if ( is_callback ) {
-    callback();
-  }
+  // if ( is_callback ) {
+  //   callback();
+  // }
   // 2. 3항 연산자를 사용한 조건 처리
-  is_callback ? callback() : null;
+  // is_callback ? callback() : null;
   // 3. 논리 연산자를 사용한 조건 처리
   is_callback && callback();
   // 4. swich 문을 사용하는 조건 처리
-  switch( is_callback ) {
-    case true: callback();
-    break;
-    case false: console.info('함수를 전달할 경우 콜백 처리됩니다.');
-  }
+  // switch( is_callback ) {
+  //   case true: callback();
+  //   break;
+  //   case false: console.info('함수를 전달할 경우 콜백 처리됩니다.');
+  // }
 }
 
 // 경우 1. 함수 인자가 있을 경우
 localScope(function() {
   console.log('localScope 함수의 코드가 모두 실행된 다음 전달된 함수 실행');
+  console.log('%c------------------------------', 'color: #3d9a21');
 });
 
 // 경우 2. 함수 인자가 없을 경우
 localScope();
+
+
+// --------------------------------------------------------------------------------
+
+// 함수가 일급객체인 이유 2
+// 함수는 함수를 인자로 받을 수 있을 뿐만 아니라, 함수를 반환(return)할 수도 있다.
+
+function returnFunction (data) {
+  var a = data[0];
+  var b = data[1];
+  var c = function(){ return a + b; };
+  return c;
+}
+
+var sum = returnFunction([ 10, 20 ]);
+
+console.log( typeof sum ); // 'function'
+
+console.log( sum() ); // 30
