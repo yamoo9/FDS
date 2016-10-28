3f6879ea9f18b3050bd613ab2dca40f7837f0648
Jee Hoon <yamoo9@naver.com>
2016-10-28 13:43:23 +0900

    hash styling script

--- 2nd/DAY20/bom+dom.js
+++ 2nd/DAY20/bom+dom.js
@@ -100,7 +100,7 @@
   // "DOM을 깨우치다."
 
 
-}(this));
+}); // (this)
 
 
 
@@ -110,14 +110,12 @@
   var document   = global.document;
   var location   = global.location;
   var setTimeout = global.setTimeout;
-  var bg_color   = '#8b73ba';
-  var duration   = 1000; // 1000ms === 1s
+  var bg_color   = '#fef9d0';
+  var duration   = 3000; // 1000ms === 1s
 
   // 페이지 로드 시에 주소창의 hash 값을 가져와서
   // 해당 요소를 찾아서 스타일링한다.
   var stylingHashElement = function () {
-
-    // var target_element, hash = location.hash;
 
     var hash = location.hash;
     var target_element = null;
@@ -129,15 +127,19 @@
     if ( target_element ) {
       // 스타일 추가
       target_element.style.background = bg_color;
+      target_element.style.transition = 'background 0.3s';
       // 특정 시간이 지나면 스타일 제거
-      setTimeout(removeStyleTargetElement, duration);
+      // setTimeout(removeStyleTargetElement, duration);
+      setTimeout(function() {
+        target_element.style.background = 'transparent';
+      }, duration);
     }
 
   };
 
-  var removeStyleTargetElement = function() {
-    console.log('try setTimeout');
-  };
+  // var removeStyleTargetElement = function() {
+  //   console.log('try setTimeout');
+  // };
 
   // window 객체의 load 이벤트가 발생하면
   // styleHashElement 함수를 실행하라.
