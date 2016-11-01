64338fee1072ff9d57c40cdc4215e79c47603941
Jee Hoon <yamoo9@naver.com>
2016-11-01 11:22:17 +0900

    changePositionStrongElement()

--- 2nd/DAY22/src/js/dom-api.js
+++ 2nd/DAY22/src/js/dom-api.js
@@ -3,7 +3,7 @@
 (function(global){
   'use strict';
 
-  // 선택 -------------------------------------------------------
+  // 선택 ---------------------------------------------------------------------------------
 
   // (ELEMENT_NODE || document).getElementById();         // ELEMENT_NODE
   // (ELEMENT_NODE || document).getElementsByTagName();   // NODE_LIST (Like Array Object)
@@ -11,7 +11,7 @@
   // (ELEMENT_NODE || document).querySelector();          // ELEMENT_NODE
   // (ELEMENT_NODE || document).querySelectorAll();       // NODE_LIST (Like Array Object)
 
-  // 탐색 -------------------------------------------------------
+  // 탐색 ---------------------------------------------------------------------------------
 
   // PARENT_NODE: .parentNode
 
@@ -23,18 +23,18 @@
   // .nextSibling, .previousSibling
   // .nextElementSibling, .previousElementSibling
 
-  // 삽입(추가) --------------------------------------------------
+  // 삽입(추가) ----------------------------------------------------------------------------
 
   // PARENT_NODE.appendChild(CHILD_NODE)
   // TARGET_NODE.appendChild(CHILD_NODE)
   // TARGET_NODE.parentNode.insertBefore(INSERT_NODE, TARGET_NODE)
 
-  // 제거 -------------------------------------------------------
+  // 제거 ---------------------------------------------------------------------------------
 
   // PARENT_NODE.removeChild(CHILD_NODE)
   // CHILD_NODE.parentNode.removeChild(CHILD_NODE)
 
-  // 대체 -------------------------------------------------------
+  // 대체 ---------------------------------------------------------------------------------
 
   // NODE를 다른 NODE와 위치를 서로 변경하는 것이 아닙니다.
   // 기존 NODE를 제거(반환)한 후, 새로운 NODE를 그 위치에 대체시키는 것입니다.
@@ -50,26 +50,39 @@
 
   for ( var i=0, l=gnb_links.length; i<l; i++ ) {
     var gnb_link = gnb_links[i];
-    gnb_link.onclick = replaceStrongElement;
+    // gnb_link.onclick = replaceStrongElement;
+    gnb_link.onclick = changePositionStrongElement;
+  }
+
+  function changePositionStrongElement() {
+    var parent = this.parentNode;
+    var target_strong = target_p.querySelector('strong');
+    var target_strong_next_sibling = target_strong.nextSibling;
+    // console.log(target_strong_next_sibling);
+    var replaced_node = parent.replaceChild(target_strong, this);
+    // console.log(replaced_node);
+    // 텍스트 노드 앞에 제거된 <a> 요소를 삽입힌다.
+    target_strong_next_sibling.parentNode.insertBefore(this, target_strong_next_sibling);
   }
 
   function replaceStrongElement() {
+
     // 대체 요소 탐색(선택)
     var target_strong = target_p.querySelector('strong');
-    console.log('target_strong:', target_strong);
+    // console.log('target_strong:', target_strong);
 
     // 대체 요소를 자신의 위치로 대체
     // <a> 요소 클릭하면, 해당 <a> 요소 위치에 <strong> 요소가 대체된다.
     var replaced_el = this.parentNode.replaceChild(target_strong, this);
-
-    console.log('replaced_el:', replaced_el);
+    // console.log('replaced_el:', replaced_el);
 
     // 웹 브라우저 기본 동작 차단
     // 오래 전 방법
     return false;
     // 이 아래 코드는 실행 X
+
   }
 
-  // 복사 -------------------------------------------------------
+  // 복사 ---------------------------------------------------------------------------------
 
 })(this);
