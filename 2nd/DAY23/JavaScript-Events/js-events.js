f86bcb97cb2530c88f5c544da9faf01fdbfdbb84
Jee Hoon <yamoo9@naver.com>
2016-11-02 13:54:12 +0900

    window 객체의 이벤트 load, resize

--- 2nd/DAY23/JavaScript-Events/js-events.js
+++ 2nd/DAY23/JavaScript-Events/js-events.js
@@ -2,6 +2,14 @@
 (function(global){
   'use strict';
 
+  // Function.prototype 객체의 능력(Methods)
+  // call, apply, bind
 
+  // ES 3
+  // Function.prototype.call
+  // Function.prototype.apply
+
+  // ES 5
+  // Function.prototype.bind
 
 })(this);
