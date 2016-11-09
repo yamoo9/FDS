f6c231ca1cceff1fe52e2ea766e8d2c6b436acf2
Jee Hoon <yamoo9@naver.com>
2016-11-09 17:45:53 +0900

    스크롤 무빙 완성

--- 2nd/DAY27/scrollMoving/js/scroll_moving.js
+++ 2nd/DAY27/scrollMoving/js/scroll_moving.js
@@ -1,14 +1,26 @@
 jQuery(function($) {
   'use strict';
 
-  var $win = $(window);
+  // var $win = $(window);
+  var $doc = $('html, body');
+  var $scroll_menu_ul = $('.scroll-menu ul');
 
   $('.scroll-menu a').on('click', function(evt) {
-    var scroll_menu_ul_height = window.parseInt($(this).find('ul').css('height'),10);
+    evt.preventDefault();
+    var scroll_menu_ul_height = window.parseInt( $scroll_menu_ul.css('height'),10 );
     var target_id             = this.getAttribute('href');
     var $target               = $(target_id);
 
-    $win.scrollTop($target.offset().top - scroll_menu_ul_height);
+    // 목적지로 바로 점핑
+    // window.scrollTo(0,$target.offset().top - scroll_menu_ul_height);
+
+    // 목적지로 수직 방향 스크롤 애니메이션
+    // eading 참고:
+    // http://gsgd.co.uk/sandbox/jquery/easing/
+    // http://easings.net/ko
+    $doc.animate({
+      'scrollTop': $target.offset().top - scroll_menu_ul_height
+    }, 1500, 'easeOutElastic');
   });
 
 });
