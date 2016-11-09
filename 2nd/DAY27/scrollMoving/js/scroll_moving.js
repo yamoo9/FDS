ce2df8057806065c5d848b67ca28b65201a2ea75
Jee Hoon <yamoo9@naver.com>
2016-11-09 17:28:36 +0900

    스크롤 메뉴 링크 클릭 이벤트 설정

--- 2nd/DAY27/scrollMoving/js/scroll_moving.js
+++ 2nd/DAY27/scrollMoving/js/scroll_moving.js
@@ -1,21 +1,14 @@
 jQuery(function($) {
   'use strict';
 
-  $('.scroll-menu').on('click', function(evt) {
-    var target = evt.target;
-    if ( target.nodeName === 'A' ) {
-      evt.preventDefault();
-    }
-    evt.stopPropagation();
+  var $win = $(window);
 
-    // console.log('#'+target.href.split('#')[1]);
-    // console.log('target:', target.getAttribute('href'));
-    // console.log('$:', $(target).attr('href'));
+  $('.scroll-menu a').on('click', function(evt) {
+    var scroll_menu_ul_height = window.parseInt($(this).find('ul').css('height'),10);
+    var target_id             = this.getAttribute('href');
+    var $target               = $(target_id);
 
-    var target_id = target.getAttribute('href');
-
-    var $target = $(target_id);
-    console.log('$target:', $target.offset().top);
+    $win.scrollTop($target.offset().top - scroll_menu_ul_height);
   });
 
 });
