618e717eba82433943a1cc6f7536f41992eb2b8f
Jee Hoon <yamoo9@naver.com>
2016-11-09 11:03:12 +0900

    .css() 메소드 정리(복습)

--- 2nd/DAY27/js/jquery-styling.js
+++ 2nd/DAY27/js/jquery-styling.js
@@ -2,4 +2,34 @@
 (function(global, $){
   'use strict';
 
+  // jQuery 인스턴스 메소드 === jQuery.fn의 메소드
+  // jQuery {} 인스턴스 객체를 만들어야 사용 가능한 메소드
+  // 인라인 스타일을 설정하는 jQuery 인스턴스 메소드 .css()
+
+  var $container = $('.demo-container');
+  // 1. 스타일 속성 값을 가져오기
+  // console.log($container.css('display'));
+  // 2. 값을 전달하면 인라인 스타일로 해당 요소에 적용
+  $container.css('background', 'linear-gradient(#777, #000)');
+  // 3. 복수의 속성들을 일괄적으로 적용
+  $container.css({
+    'flex-direction': 'column',
+    'flex-wrap': 'wrap',
+    'align-content': 'flex-start'
+  });
+  // 4. 해당 요소로부터 복수의 속성 값을 배열로 전달해서, 객체로 반환 받고자 할 경우
+  var container_props = $container.css(['flex-flow', 'justify-content']);
+
+  $('.album img').css({
+    'display': function(idx, prop){
+      if ( idx === 2 && !container_props.display ) {
+        container_props.display = prop;
+      } else {
+        prop = 'inline-block';
+        return prop;
+      }
+    }
+  });
+
+
 })(this, this.jQuery);
