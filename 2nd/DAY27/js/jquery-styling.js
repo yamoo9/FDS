2e67acbd66d2460aff58c61dbd1256564259abed
Jee Hoon <yamoo9@naver.com>
2016-11-09 11:37:57 +0900

    movePlayStop CD

--- 2nd/DAY27/js/jquery-styling.js
+++ 2nd/DAY27/js/jquery-styling.js
@@ -20,7 +20,9 @@
   // 4. 해당 요소로부터 복수의 속성 값을 배열로 전달해서, 객체로 반환 받고자 할 경우
   var container_props = $container.css(['flex-flow', 'justify-content']);
 
-  $('.album img').css({
+  var $album = $('.album');
+
+  $('img', $album).css({
     'display': function(idx, prop){
       if ( idx === 2 && !container_props.display ) {
         container_props.display = prop;
@@ -30,6 +32,43 @@
       }
     }
   });
+
+  // 일반적인 방법
+  // $album.on('click', moveDisk);
+  // $album.on('mouseenter', playDisk);
+  // $album.on('mouseleave', stopDisk);
+
+  // 메소드 체이닝
+  // $album
+  //   .on('click', moveDisk)
+  //   .on('mouseenter', playDisk)
+  //   .on('mouseleave', stopDisk);
+
+  // 객체 리터럴 활용
+  $album.on({
+    'click'     : moveDisk,
+    'mouseenter': playDisk,
+    'mouseleave': stopDisk
+  });
+
+  var is_click = false;
+
+  function moveDisk() {
+    is_click = !is_click;
+    $(this).find('.album-disk').addClass('move');
+  }
+
+  function playDisk() {
+    if (is_click === true) {
+      $(this).find('.album-disk').addClass('play').removeClass('move');
+    }
+  }
+
+  function stopDisk() {
+    if (is_click === true) {
+      $(this).find('.album-disk').removeClass('play').addClass('stop');
+    }
+  }
 
 
 })(this, this.jQuery);
