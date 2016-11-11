a6ea2291af43a03ef3cdb9c732c7b29984b9c4b5
Jee Hoon <yamoo9@naver.com>
2016-11-11 16:56:33 +0900

    인디케이터 클릭에 따른 컨텐츠 변경

--- 2nd/DAY28/UI Carousel/js/ui-carousel.js
+++ 2nd/DAY28/UI Carousel/js/ui-carousel.js
@@ -1,5 +1,17 @@
 /*! ui-carousel.js © yamoo9.net, 2016 */
 
+/*! jquery.radioClass.js © yamoo9.net, 2016 */
+(function(global, $){
+  'use strict';
+
+  $.fn.radioClass = function(class_name) {
+    this.siblings('.'+class_name).removeClass(class_name);
+    this.addClass(class_name);
+    return this;
+  };
+
+})(this, this.jQuery);
+
 // -----------------------------------------
 // TODO:
 // -----------------------------------------
@@ -14,14 +26,16 @@
 (function(global, $){
   'use strict';
 
+  // -------------------------------------------------------------
   // ui_Carousel 생성자 함수 정의
   function ui_Carousel(selector) {
     // 초기 생성자 함수의 역할
-    this.$carousel          = null;
-    this.$carousel_headline = null;
-    this.$carousel_tablist  = null;
-    this.$carousel_tabs     = null;
-    this.$button_group      = null;
+    this.$active_index               = 0;
+    this.$carousel                   = null;
+    this.$carousel_headline          = null;
+    this.$carousel_tablist           = null;
+    this.$carousel_tabs              = null;
+    this.$button_group               = null;
     this.$carousel_tabpanel_contents = null;
 
     // this는 생성된 ui_Carousel 객체 인스턴스
@@ -29,8 +43,12 @@
     this.events();
   }
 
+  // -------------------------------------------------------------
   // ui_Carousel.prototype 객체 정의(확장)
-  ui_Carousel.prototype.init = function(selector) {
+  // ui_Carousel.prototype 단축 이름 fn 설정
+  ui_Carousel.fn = ui_Carousel.prototype;
+
+  ui_Carousel.fn.init = function(selector) {
     this.$carousel          = $(selector);
     this.$carousel_headline = this.$carousel.children(':header:first');
     this.$carousel_tablist  = this.$carousel.children('ul').wrap('<div/>').parent();
@@ -42,7 +60,7 @@
     // this.settingAria();
   };
 
-  ui_Carousel.prototype.createPrevNextButtons = function() {
+  ui_Carousel.fn.createPrevNextButtons = function() {
     var button_group = ['<div>',
       '<button type="button"></button>',
       '<button type="button"></button>',
@@ -50,7 +68,7 @@
     this.$button_group = $(button_group).insertAfter( this.$carousel_tablist );
   };
 
-  ui_Carousel.prototype.createTabpanelWrapper = function() {
+  ui_Carousel.fn.createTabpanelWrapper = function() {
     var widget = this;
     widget.$carousel_tabpanel_contents = widget.$carousel.children().last().children();
     $.each(widget.$carousel_tabpanel_contents, function(index) {
@@ -59,7 +77,7 @@
     });
   };
 
-  ui_Carousel.prototype.settingClass = function() {
+  ui_Carousel.fn.settingClass = function() {
     this.$carousel.addClass('ui-carousel');
     this.$carousel_headline.addClass('ui-carousel-headline');
     this.$carousel_tablist.addClass('ui-carousel-tablist');
@@ -71,25 +89,28 @@
     this.$carousel_tabpanel_contents.closest('div').addClass('ui-carousel-tabpanel-wrapper');
   };
 
-  ui_Carousel.prototype.events = function() {
+  ui_Carousel.fn.events = function() {
     var widget = this;
     var $tabs = widget.$carousel_tabs;
-
     $.each($tabs, function(index) {
       var $tab = $tabs.eq(index);
-      $tab.on('click', $.proxy(widget.viewTabpanel, $tab, index));
+      $tab.on('click', $.proxy(widget.viewTabpanel, widget, index));
     });
   };
 
-  ui_Carousel.prototype.viewTabpanel = function(index, e) {
-    // index에 해당되는 탭패널 활성화
+  ui_Carousel.fn.viewTabpanel = function(index, e) {
+    // 사용자가 클릭을 하는 행위가 발생하면 이벤트 객체를 받기 때문에
+    // 조건 확인을 통해 브라우저의 기본 동작 차단
+    if (e) { e.preventDefault(); }
+    // 활성화된 인덱스를 사용자가 클릭한 인덱스로 변경
+    this.$active_index = index;
     // 인디케이터 라디오클래스 활성화
-    if ( this.jquery && this.hasClass('ui-carousel-tab') ) {
-      e.preventDefault();
-    }
-    console.log(index);
+    this.$carousel_tabs.eq(this.$active_index).parent().radioClass('active');
+    // index에 해당되는 탭패널 활성화
+    this.$carousel_tabpanel_contents.eq(this.$active_index).parent().radioClass('active');
   };
 
+  // 모듈 외부에 공개
   global.ui_Carousel = ui_Carousel;
 
 })(this, this.jQuery);
