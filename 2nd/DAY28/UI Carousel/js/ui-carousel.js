5afd1956bb4f441335a5d6498644eed97d18718c
Jee Hoon <yamoo9@naver.com>
2016-11-11 18:05:01 +0900

    이전, 다음 탐색 버튼 감지

--- 2nd/DAY28/UI Carousel/js/ui-carousel.js
+++ 2nd/DAY28/UI Carousel/js/ui-carousel.js
@@ -29,18 +29,22 @@
   // -------------------------------------------------------------
   // ui_Carousel 생성자 함수 정의
   function ui_Carousel(selector) {
-    // 초기 생성자 함수의 역할
+    // this는 생성된 ui_Carousel 객체 인스턴스
+
+    // 초기 생성자 함수 내에 설정된 초기 값
     this.carousel_radio              = 700 / 1200;
     this.active_index                = 0;
+    this.carousel_mask_width         = 0;
+    this.using_animation             = true;
     this.$carousel                   = null;
     this.$carousel_headline          = null;
     this.$carousel_tablist           = null;
     this.$carousel_tabs              = null;
     this.$carousel_button_group      = null;
     this.$carousel_tabpanel_contents = null;
-
-    // this는 생성된 ui_Carousel 객체 인스턴스
+    // 초기 설정
     this.init(selector);
+    // 이벤트 연결
     this.events();
   }
 
@@ -50,14 +54,18 @@
   ui_Carousel.fn = ui_Carousel.prototype;
 
   ui_Carousel.fn.init = function(selector) {
+    // 캐러셀 내부 구성 요소 참조
     this.$carousel          = $(selector);
     this.$carousel_headline = this.$carousel.children(':header:first');
     this.$carousel_tablist  = this.$carousel.children('ul').wrap('<div/>').parent();
     this.$carousel_tabs     = this.$carousel_tablist.find('a');
-    // 역할별 스타일링 되는 클래스 설정
+    // 동적으로 캐러셀 구조 생성/추가
     this.createPrevNextButtons();
     this.createTabpanelWrapper();
+    // 역할별 스타일링 되는 클래스 설정
     this.settingClass();
+    this.settingSliding();
+    // WAI-ARIA 설정
     this.settingAria();
   };
 
@@ -86,8 +94,12 @@
     this.$carousel_button_group.addClass('ui-carousel-button-group');
     this.$carousel_button_group.children().first().addClass('ui-carousel-prev-button');
     this.$carousel_button_group.children().last().addClass('ui-carousel-next-button');
-    this.$carousel_tabpanel_contents.parent().addClass('ui-carousel-tabpanel');
+    this.$carousel_tabpanel_contents.parent().addClass('ui-carousel-tabpanel trans');
     this.$carousel_tabpanel_contents.closest('div').addClass('ui-carousel-tabpanel-wrapper');
+  };
+
+  ui_Carousel.fn.settingSliding = function() {
+    this.carousel_mask_width = this.$carousel.find('.ui-carousel-tabpanel-wrapper').width();
   };
 
   ui_Carousel.fn.settingAria = function() {
@@ -103,9 +115,9 @@
     // buttons event
     $buttons.on('click', function() {
       if ( this.className === 'ui-carousel-prev-button' ) {
-        widget.viewTabpanel(widget.active_index - 1);
+        widget.nextPanel();
       } else {
-        widget.viewTabpanel(widget.active_index + 1);
+        widget.prevPanel();
       }
     });
 
@@ -117,27 +129,41 @@
 
     // resize event
     $(global).on('resize', function() {
+      widget.settingSliding();
       $carousel.height( $carousel.width() * widget.carousel_radio );
     });
   };
 
-  ui_Carousel.fn.viewTabpanel = function(index, e) {
+  ui_Carousel.fn.nextPanel = function() {
+    this.viewTabpanel(this.active_index + 1);
+  };
+
+  ui_Carousel.fn.prevPanel = function() {
+    this.viewTabpanel(this.active_index - 1);
+  };
+
+  ui_Carousel.fn.viewTabpanel = function(index, animate, e) {
+    animate = animate || this.using_animation;
     // 사용자가 클릭을 하는 행위가 발생하면 이벤트 객체를 받기 때문에
     // 조건 확인을 통해 브라우저의 기본 동작 차단
     if (e) { e.preventDefault(); }
+    var pressedButton = this.active_index > index ? 'next' : 'prev';
     // 활성화된 인덱스를 사용자가 클릭한 인덱스로 변경
     this.active_index = index;
+    var carousel_tabs_max = this.$carousel_tabs.length - 1;
+    // 처음 또는 마지막 인덱스에 해당할 경우 마지막 또는 처음으로 변경하는 조건 처리
     if ( this.active_index < 0 ) {
-      this.active_index = this.$carousel_tabs.length - 1;
+      this.active_index = carousel_tabs_max; }
+    if ( this.active_index > carousel_tabs_max ) {
+      this.active_index = 0; }
+    if (animate) {
+      console.log(this.carousel_mask_width, pressedButton);
+    } else {
+      // 인디케이터 라디오클래스 활성화
+      this.$carousel_tabs.eq(this.active_index).parent().radioClass('active');
+      // index에 해당되는 탭패널 활성화
+      this.$carousel_tabpanel_contents.eq(this.active_index).parent().radioClass('active');
     }
-    if ( this.active_index > this.$carousel_tabs.length - 1 ) {
-      this.active_index = 0;
-    }
-    console.log(this.active_index);
-    // 인디케이터 라디오클래스 활성화
-    this.$carousel_tabs.eq(this.active_index).parent().radioClass('active');
-    // index에 해당되는 탭패널 활성화
-    this.$carousel_tabpanel_contents.eq(this.active_index).parent().radioClass('active');
   };
 
   // 모듈 외부에 공개
