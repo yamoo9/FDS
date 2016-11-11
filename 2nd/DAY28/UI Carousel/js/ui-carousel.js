74c5d47724f945c22abde0e0281db99fe1a3d56a
Jee Hoon <yamoo9@naver.com>
2016-11-11 15:23:45 +0900

    ui_Carousel 생성자 함수 + 프로토타입 객체

--- 2nd/DAY28/UI Carousel/js/ui-carousel.js
+++ 2nd/DAY28/UI Carousel/js/ui-carousel.js
@@ -10,3 +10,50 @@
 // 5. 콘텐츠 뷰 자동 재생/정지 기능 구현
 // 6. 플러그인 구현
 // -----------------------------------------
+
+(function(global, $){
+  'use strict';
+
+  // ui_Carousel 생성자 함수 정의
+  function ui_Carousel(selector) {
+    // 초기 생성자 함수의 역할
+    this.$carousel          = null;
+    this.$carousel_headline = null;
+    this.$carousel_tablist  = null;
+    this.$carousel_tabs     = null;
+
+    // this는 생성된 ui_Carousel 객체 인스턴스
+    this.init(selector);
+    this.events();
+  }
+
+  // ui_Carousel.prototype 객체 정의(확장)
+  ui_Carousel.prototype.init = function(selector) {
+    this.$carousel          = $(selector);
+    this.$carousel_headline = this.$carousel.children(':header:first');
+    this.$carousel_tablist  = this.$carousel.children('ul').wrap('<div/>').parent();
+    this.$carousel_tabs     = this.$carousel_tablist.find('a');
+    // 역할별 스타일링 되는 클래스 설정
+    this.settingClass();
+    // this.settingAria();
+  };
+
+  ui_Carousel.prototype.settingClass = function() {
+    this.$carousel.addClass('ui-carousel');
+    this.$carousel_headline.addClass('ui-carousel-headline');
+    this.$carousel_tablist.addClass('ui-carousel-tablist');
+    this.$carousel_tabs.addClass('ui-carousel-tab');
+  };
+
+  ui_Carousel.prototype.events = function() {
+    this.$carousel_tabs.on('click', this.viewTabpanel);
+  };
+
+  ui_Carousel.prototype.viewTabpanel = function(index) {
+    // index에 해당되는 탭패널 활성화
+    // 인디케이터 라디오클래스 활성화
+  };
+
+  global.ui_Carousel = ui_Carousel;
+
+})(this, this.jQuery);
