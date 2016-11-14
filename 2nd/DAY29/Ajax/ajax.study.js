717d0086b1101756c2105ddad47bee899953c122
Jee Hoon <yamoo9@naver.com>
2016-11-14 13:10:49 +0900

    비동기 통신일 경우는 아래 코드가 바로 실행

--- 2nd/DAY29/Ajax/ajax.study.js
+++ 2nd/DAY29/Ajax/ajax.study.js
@@ -8,28 +8,46 @@
   // 전달인자 1. `통신 방법`
   // 전달인자 2. `파일 경로`
   // 전달인자 3. `비동기 설정` [비동기:true, 동기:false]
-  xhr.open('GET', './data/data.doc', false);
+  xhr.open('GET', './data/data.txt'); // default value: true -> 비동기(Async)
 
   // 버튼 클릭 이벤트 핸들링
   var ajax_call_btn         = document.querySelector('.call-ajax-data');
   var ajax_call_data_result = document.querySelector('.ajax-data-result');
 
-  ajax_call_btn.addEventListener('click', callAjaxData);
+  // 동기/비동기 방식에 대한 이해
+  xhr.send(); // 데이터 요청.................
 
-  function callAjaxData() {
-    xhr.send();
-    console.log('콜백 Ajax 데이터');
-    // 200: success, 404: client error
-    console.log('xhr.status:', xhr.status);
-    if ( xhr.status === 200 ) {
-      console.log('데이터 통신에 성공했습니다. :-)');
-      ajax_call_data_result.innerHTML = xhr.responseText;
-    } else {
-      console.error('데이터 통신에 실패했습니다. :-(');
-      ajax_call_data_result.innerHTML = '데이터 통신에 실패했습니다. :-(';
-      ajax_call_data_result.classList.add('error');
-    }
+  // 비동기 통신일 경우는 아래 코드가 바로 실행
+  console.log('xhr.status:', xhr.status); // 0
+
+  if ( xhr.status === 200 ) {
+    console.log('데이터 통신에 성공했습니다. :-)');
+    console.log('xhr.response:', xhr.response);
+    console.log('xhr.responseText:', xhr.responseText);
+    // console.log('xhr.responseType:', xhr.responseType);
+    ajax_call_data_result.innerHTML = xhr.responseText;
+  } else {
+    console.error('데이터 통신에 실패했습니다. :-(');
+    ajax_call_data_result.innerHTML = '데이터 통신에 실패했습니다. :-(';
+    ajax_call_data_result.classList.add('error');
   }
+
+  // ajax_call_btn.addEventListener('click', callAjaxData);
+
+  // function callAjaxData() {
+  //   xhr.send();
+  //   console.log('콜백 Ajax 데이터');
+  //   // 200: success, 404: client error
+  //   console.log('xhr.status:', xhr.status);
+  //   if ( xhr.status === 200 ) {
+  //     console.log('데이터 통신에 성공했습니다. :-)');
+  //     ajax_call_data_result.innerHTML = xhr.responseText;
+  //   } else {
+  //     console.error('데이터 통신에 실패했습니다. :-(');
+  //     ajax_call_data_result.innerHTML = '데이터 통신에 실패했습니다. :-(';
+  //     ajax_call_data_result.classList.add('error');
+  //   }
+  // }
 
   // 2초가 지난 뒤에 send()
   // global.setTimeout(function() {
