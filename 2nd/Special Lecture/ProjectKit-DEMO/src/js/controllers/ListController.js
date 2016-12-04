65e4711c79b7b9d563c42c9a677da19938f3261b
Jee Hoon <yamoo9@naver.com>
2016-12-05 06:08:47 +0900

    Recording 02: 클라이언트 환경에서의 정렬 기능 추가

--- 2nd/Special Lecture/ProjectKit-DEMO/src/js/controllers/ListController.js
+++ 2nd/Special Lecture/ProjectKit-DEMO/src/js/controllers/ListController.js
@@ -192,12 +192,26 @@ angular
     ];
 
     // 컨트롤러 $scope 객체의 속성
-    $scope.search = {};
+    // $scope.search = {};
+    $scope.search         = '';
+    $scope.order          = 'title';
     $scope.selected_movie = null;
 
     // 컨트롤러 $scope 객체의 메소드
     $scope.selectMovie = function(movie) {
       $scope.selected_movie = movie;
+    };
+
+    // 엄격하게 일치하는 검색 기능
+    $scope.sensitiveSearch = function(movie) {
+      var search = $scope.search;
+      if ( search ) {
+        return movie.title.indexOf(search) === 0 ||
+               movie.director.indexOf(search) === 0 ||
+               movie.year.indexOf(search) === 0 ||
+               movie.category.indexOf(search) === 0;
+      }
+      return true;
     };
 
   }]);
