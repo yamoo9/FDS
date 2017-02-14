250beed79b1e29f91cfb6e93d0e1e174506ceca3
Jee Hoon <yamoo9@naver.com>
2017-02-14 17:37:50 +0900

    도전과제 html, css 파일 추가

---
+++ LECTURE/DAY_08/app.js
@@ -0,0 +1,17 @@
+// JavaScript 클라이언트 사이트 환경
+// BOM(Browser Object Model)
+// Global Object => window
+// .alert()
+// 문서 (document)
+// .documentElement => <html> root element
+// 로케이션 (location)
+// .href
+// .hash
+// .reload()
+// 히스토리 (history)
+// .back()
+// .go()
+// .forward()
+// 스크린 (screen)
+// .width
+// .availWidth
