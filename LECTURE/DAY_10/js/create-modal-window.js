5ce38432f321fc9dbb35cfd3b86b0d963b83adc0
Jee Hoon <yamoo9@naver.com>
2017-02-17 16:38:40 +0900

    모달 윈도우 코드 완성

--- LECTURE/DAY_10/js/create-modal-window.js
+++ LECTURE/DAY_10/js/create-modal-window.js
@@ -1,3 +1,6 @@
+/*! create-modal-window.js © yamoo9.net, 2017 */
+
+var body      = document.body;
 // 사용자가 클릭할 버튼을 찾아온다.
 var modal_btn = document.querySelector('.button__create-modal');
 // 사용자가 버튼을 클릭하면
@@ -6,6 +9,24 @@ modal_btn.onclick = createModalWindow;
 function createModalWindow() {
   // 동적으로 모달 윈도우를 생성하여 화면에 표시한다.
   // console.log('created modal window');
-  //
-  // document.createElement('');
+  // #1
+  var modal = document.createElement('div');
+  modal.setAttribute('class', 'modal-window');
+  // #2
+  var modal_headline         = document.createElement('h1');
+  var modal_headline_content = document.createTextNode('Modal Window');
+  modal_headline.appendChild(modal_headline_content);
+  modal_headline.setAttribute('class', 'modal-window__headline');
+  // #3
+  var modal_btn         = document.createElement('button');
+  var modal_btn_content = document.createTextNode('X');
+  modal_btn.appendChild(modal_btn_content);
+  modal_btn.setAttribute('type', 'button');
+  modal_btn.setAttribute('class', 'modal-window__close-button');
+  modal_btn.setAttribute('aria-label', 'Close Modal Window');
+  // #4
+  modal.appendChild(modal_headline);
+  modal.appendChild(modal_btn);
+  // #5
+  body.appendChild(modal);
 }
