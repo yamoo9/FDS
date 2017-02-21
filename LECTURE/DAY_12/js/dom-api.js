75fefc6682e8468dd725365d73d68ce9a21fb4cb
Jee Hoon <yamoo9@naver.com>
2017-02-21 12:59:08 +0900

    switchNode() 코드 구문 추가

--- LECTURE/DAY_12/js/dom-api.js
+++ LECTURE/DAY_12/js/dom-api.js
@@ -28,26 +28,49 @@ var section_b_lis = queryAll('li', section_b);
 // console.log('section_b_lis:', section_b_lis);
 
 // assignActiveClass 함수 정의
-var assignActiveClass = function() {
+var assignActiveClass = function(clicked_target) {
+  // console.log('assignActiveClass:', clicked_target);
+  // return; // 함수 종료
+  // ----------------------------------------------------
   // 클릭된 li 요소노드의 부모노드로 접근하여,
   // 부모노드의 자식 중 활성화된 active 클래스를 가진 요소를 찾아
   // 해당 요소에 class 속성 값을 제거한다.
   var actived_li = null;
-  var parent = this.parentNode;
+  var parent = clicked_target.parentNode;
   actived_li = query('.active', parent);
   if(actived_li !== null) {
     actived_li.removeAttribute('class');
   }
-  // this === 사용자가 클릭한 li 요소노드를 가리킨다.
-  this.setAttribute('class', 'active');
+  // clicked_target === 사용자가 클릭한 li 요소노드를 가리킨다.
+  clicked_target.setAttribute('class', 'active');
 };
+
+var detectActivateState = function(child_node) {
+  var cross_actived = null;
+  if ( child_node.parentNode === section_a ) {
+    // 사용자가 클릭한 li의 부모가 section-A인 경우
+    // console.log('A');
+    cross_actived = query('.active', section_b);
+  } else {
+    // 사용자가 클릭한 li의 부모가 section-B인 경우
+    // console.log('B');
+    cross_actived = query('.active', section_a);
+  }
+  console.log('cross_actived:', cross_actived);
+};
+
+var switchNode = function() {
+  // console.log('switchNode:', this);
+  detectActivateState(this.parentNode);
+  assignActiveClass(this);
+}
 
 // 반복문을 사용하여 수집된 li 요소노드에 일괄적으로 이벤트를 연결
 for ( var i=0; i<section_a_lis.length; i=i+1 ) {
   var section_a_li = section_a_lis.item(i);
-  section_a_li.onclick = assignActiveClass;
+  section_a_li.onclick = switchNode;
 }
-for ( var i=0; i<section_a_lis.length; i=i+1 ) {
-  var section_a_li = section_a_lis.item(i);
-  section_a_li.onclick = assignActiveClass;
+for ( var i=0; i<section_b_lis.length; i=i+1 ) {
+  var section_b_li = section_b_lis.item(i);
+  section_b_li.onclick = switchNode;
 }
