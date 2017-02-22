6783a816da5f6d0496d692fb5c3c72986aa33510
Jee Hoon <yamoo9@naver.com>
2017-02-22 11:53:23 +0900

    HTML Template 활용

--- LECTURE/DAY_13/js/dom-api.js
+++ LECTURE/DAY_13/js/dom-api.js
@@ -9,5 +9,18 @@ var add_html_string = '<header class="container-header"><h1 class="site-brand" l
 
 // 문서에서 .add-HTML-btn 버튼을 찾아 이벤트를 연결해보자.
 query('.add-HTML-btn').onclick = function() {
-  demo.innerHTML = add_html_string;
+  // demo.innerHTML = add_html_string;
+
+  // 사용자가 입력한 데이터(Model) -----------------
+  // 사용자가 <input> 필드에 입력한 데이터 값을 가져와
+  // 아래 변수에 할당하시오.
+  var lang    = query('#input-language').value || 'ko';
+  var heading = query('#input-heading').value || '오늘은 눈인지 비인지...';
+
+  // 데이터를 조합할 HTML 템플릿 -------------------
+  var template = '<header class="container-header"><h1 class="site-brand" lang="'+ lang +'">'+ heading +'</h1></header>';
+
+  // Template + Model(Data) -> View 완성되어 화면에 실제 구현
+  demo.innerHTML = template;
+
 };
