62db7490c8509608dafd1042789cf25501b89896
Jee Hoon <yamoo9@naver.com>
2017-02-24 23:08:28 +0900

    CSSOM 코드 추가

--- LECTURE/DAY_14/js/dom-api.js
+++ LECTURE/DAY_14/js/dom-api.js
@@ -36,7 +36,7 @@ var demo_button = query('button');
 demo_button.classList.add('demo-button');
 
 demo_button.onclick = function () {
-  if ( this.classList.contains('on') ) {
+  if (this.classList.contains('on')) {
     this.classList.remove('on');
   } else {
     this.classList.add('on');
@@ -55,28 +55,54 @@ var _div = createElement('div');
 
 doc_frag.appendChild(_div);
 
-console.log('_div:', _div );
+console.log('_div:', _div);
 
 //console.log('doc_frag 2:', doc_frag);  // #document-fragment > div
 
 _div = createElement('span');
 
-console.log('_div:', _div );
+console.log('_div:', _div);
 
 doc_frag.appendChild(_div);
 
 //console.log('doc_frag 3:', doc_frag);
 
+// ------------------------------------------------
+// CSSOM
 
+var div = createElement('div');
+var wrapper = createElement('div');
+wrapper.classList.add('wrapper');
 
+prepend(wrapper, div);
+prepend(document.body, wrapper);
 
+wrapper.style.cssText = [
+  'position: absolute',
+  'top: 200px',
+  'left: 200px',
+  'width: 500px',
+  'height: 500px',
+  'padding: 1rem',
+  'border: 4px solid lightgray'
+].join(';');
 
+div.style.position = 'absolute';
+div.style.top = '30px';
+div.style.left = '70px';
+div.style.width = '300px';
+div.style.height = '300px';
+div.style.background = '#333';
+div.style.boxSizing = 'border-box';
 
-
-
-
-
-
-
-
-
+console.log('height:', window.parseInt(div.style.height, 10));
+console.log('width:', window.parseInt(div.style.width, 10));
+//
+div.style.padding = '10px';
+//
+console.log('width | context-box + padding-box:', div.clientWidth);
+console.log('height | context-box + padding-box:', div.clientHeight);
+//
+div.style.border = '5px solid #32d2ff';
+console.log('width | context-box + padding-box + border-box:', div.offsetWidth);
+console.log('height | context-box + padding-box + border-box:', div.offsetHeight);
