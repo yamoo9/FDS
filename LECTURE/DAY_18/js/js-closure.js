eab3b03a75e11b6435d8fa4cd6dfa002591722e3
Jee Hoon <yamoo9@naver.com>
2017-03-06 16:47:22 +0900

    클로저 반환 객체

--- LECTURE/DAY_18/js/js-closure.js
+++ LECTURE/DAY_18/js/js-closure.js
@@ -31,9 +31,54 @@ numbering(); // 3
 var counter = function() {
   // 함수 counter 공간
   var init_count = 0;
-  var countDown = function() {
+  var countDown = function(reset) {
+    if ( reset ) {
+      init_count = 0;
+      console.log('init_count:', init_count);
+    }
     // 함수 countDown 공간
-    return init_count++; // 0, 1, 2, 3, 4, 5, ...
+    return init_count++;
   };
   return countDown;
 };
+
+var countDown = counter(); // 클로저 함수 반환
+
+countDown(); // 0
+countDown(); // 1
+countDown(); // 2
+countDown(); // 3
+
+
+// 클로저 활용 예시 Part. 2
+// 함수가 아닌, 객체를 내보내는 함수
+function countMaker( init_count ) {
+  // 초기 값 설정
+  var _count = init_count || 0;
+  // 감춰진 멤버(보안 처리, 은폐)
+  var increase = function(step) {
+    _count += (step || 1);
+    return _count;
+  };
+  var decrease = function(step) {
+    _count -= (step || 1);
+    return _count;
+  };
+  var getCount = function() {
+    return _count;
+  };
+  var setCount = function(number) {
+    _count = number;
+  };
+  var resetCount = function() {
+    _count = init_count;
+  };
+  // 출력 패턴
+  return {
+    'increase' : increase,
+    'decrease' : decrease,
+    'get'      : getCount,
+    'set'      : setCount,
+    'reset'    : resetCount
+  };
+}
