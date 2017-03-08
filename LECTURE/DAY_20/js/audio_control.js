bf8e9219b98a9273b3dc3e293472cd4d7899a329
Jee Hoon <yamoo9@naver.com>
2017-03-08 17:41:41 +0900

    모듈 관리 설명 - Class Method, Instance Methods

--- LECTURE/DAY_20/js/audio_control.js
+++ LECTURE/DAY_20/js/audio_control.js
@@ -1,27 +1,7 @@
 /*! audio_control.js © yamoo9.net, 2017 */
 
-// HTMLAudioElement.prototype 객체 확장
-;(function(Audio){
-  'use strict';
-
-  if ( !Audio.prototype.stop ) {
-    Audio.prototype.stop = function() {
-      this.pause();
-      this.currentTime = 0;
-    };
-  }
-
-  if ( !Audio.prototype.getProgress ) {
-    Audio.prototype.getProgress = function(preciusion) {
-      var percent = this.currentTime / this.duration * 100;
-      return percent.toFixed( preciusion || 0 );
-    }
-  }
-
-})(HTMLAudioElement);
-
 // 모듈 패턴 (비공개|공개 설정)
-;(function(global){
+;(function(global, $){
   'use strict';
 
   // 오디오 객체 생성/속성 설정
@@ -38,16 +18,6 @@
   // 오디오 ⬅︎ 대체수단 삽입
   audio.appendChild(alt_audio_link);
 
-  // 오디오 객체 확인
-  // console.log(audio);
-
-  // 오디오객체 생성자 함수 확인
-  // 생성자 함수: HTMLAudioElement
-  // console.log(audio.constructor);
-  // 프로토타입 객체: HTMLAudioElement.prototype
-  // console.log(audio.constructor.prototype);
-  // console.log(audio.__proto__);
-
   // 오류 감지 시, 오류 처리하는 헬퍼 함수
   function validate(condition, error_message) {
     if ( condition ) { throw new Error(error_message); }
@@ -56,74 +26,11 @@
   function isAudioObject(data) {
     return data && data.constructor === HTMLAudioElement;
   }
-  // 오디오 객체를 재생(play)하는 함수
-  function playMusic(audio) {
-    validate(!isAudioObject(audio), '오디오 객체가 전달되지 않았습니다.');
-    audio.play();
-  }
-  // 재생 중인 오디오 객체를 정지(stop)하는 함수
-  function stopMusic(audio) {
-    validate(!isAudioObject(audio), '오디오 객체가 전달되지 않았습니다.');
-    // 오디오 객체 일시정지
-    // audio.pause();
-    // 현재 재생 시간 콘솔에 기록
-    // console.log('현재 재생 시간:', audio.currentTime);
-    // audio.currentTime = 0;
-    audio.stop();
-  }
-
-  // 메모이제이션 패턴
-  // 오디오 객체 재생 상태 % 반환 함수
-  function statePercent(audio_type) {
-    validate(!isAudioObject(audio_type), '오디오 객체를 전달해야 합니다.');
-    if ( !statePercent.total ) {
-      statePercent.total = audio_type.duration;
-    }
-    return Math.floor( audio_type.currentTime / statePercent.total * 100 ) + '%';
-  }
-  // 함수객체.속성
-  statePercent.total = null;
-
-  // ⬇︎
-
-  // 클로저 함수 패턴
-  var statePercentage = (function(){
-    // 클로저 영역의 지역 변수
-    var total = 0;
-    // 클로저 함수 반환
-    return function(audio_type) {
-      validate(!isAudioObject(audio_type), '오디오 객체를 전달해야 합니다.');
-      if ( !total ) {
-        total = audio_type.duration;
-      }
-      return Math.floor( audio_type.currentTime / total * 100 ) + '%';
-    };
-  })();
-
-  // 오디오 재생시간 표기 처리 함수
-  // 'mm:ss'
-  function readableDuration(seconds){
-    var min, sec;
-    seconds = Math.floor( seconds );
-    min = Math.floor( seconds / 60 );
-    min = min >= 10 ? min : '0' + min;
-    sec = Math.floor( seconds % 60 );
-    sec = sec >= 10 ? sec : '0' + sec;
-    return min + ':' + sec;
-  }
 
   // 오디오 객체 재생 가능한 시점(oncanplay)이 되면 재생(.play())
   audio.oncanplay = function() {
-    // 현재 시간 / 완료 시간
-    // .currentTime
-    // console.log('audio.currentTime:', audio.currentTime);
-    // .duration
-    // console.log('audio.duration:', audio.duration);
-    audio_time_total.innerHTML = readableDuration(audio.duration);
+    audio_time_total.innerHTML = $.getReadableTime(audio.duration);
   };
-  // 오디오 객체 재생 중인 상태를 감지하는 이벤트
-  // ontimeupdate
-  // console.log('audio.ontimeupdate:', audio.ontimeupdate);
 
   // 시크바 프로세스 문서 객체 참조
   var seekbar_progress   = document.querySelector('.audio-seekbar-progress');
@@ -132,33 +39,16 @@
 
   // 오디오 객체 재생 중, 시간 업데이트 이벤트 핸들링
   audio.ontimeupdate = function() {
-    // var current = statePercent(this); // 메모이제이션 패턴
-    // var current = statePercentage(this); // 클로저 함수 패턴
-    // console.log('current:', current);
-
-    // seekbar_progress.style.width = current;
-
     seekbar_progress.style.width = this.getProgress(3) + '%';
-    audio_time_current.innerHTML = readableDuration(this.currentTime);
-
-    // var current = this.currentTime;
-    // var total = this.duration;
-    // var percent = Math.floor(current/total * 100) + '%';
-    // console.log(percent);
+    audio_time_current.innerHTML = $.getReadableTime(this.currentTime);
   };
-
-  // 0.3초가 지나면, 재생 중인 오디오를 일시정지(.pause()) 하라.
-  // window.setTimeout(function() {
-  //   // audio.pause();
-  //   audio.stop(); // 미지원 API
-  // }, 1000);
 
   // 외부에서 접근 가능하도록 공개
   global.audio     = audio;
-  global.stopMusic = stopMusic;
-  global.playMusic = playMusic;
 
-})(window);
+})(window, window.HTMLAudioElement);
+
+
 
 (function(global){
   'use strict';
