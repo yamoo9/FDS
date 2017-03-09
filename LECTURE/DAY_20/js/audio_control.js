c4ccfc8ef56a347e60a1463916b42b1e00195be6
Jee Hoon <yamoo9@naver.com>
2017-03-09 09:34:18 +0900

    bulma.css 이름 변경 / audio_control 코드 정리

--- LECTURE/DAY_20/js/audio_control.js
+++ LECTURE/DAY_20/js/audio_control.js
@@ -1,6 +1,26 @@
 /*! audio_control.js © yamoo9.net, 2017 */
 
 // 모듈 패턴 (비공개|공개 설정)
+;(function(global){
+  'use strict';
+
+  // 오류 감지 시, 오류 처리하는 헬퍼 함수
+  function validate(condition, error_message) {
+    if ( condition ) { throw new Error(error_message); }
+  }
+  // 오디오 객체인지 체크하는 헬퍼 함수
+  function isAudioObject(data) {
+    return data && data.constructor === HTMLAudioElement;
+  }
+
+  // 외부 공개
+  global.validate = validate;
+  global.isAudioObject = isAudioObject;
+
+})(window);
+
+
+
 ;(function(global, $){
   'use strict';
 
@@ -18,24 +38,10 @@
   // 오디오 ⬅︎ 대체수단 삽입
   audio.appendChild(alt_audio_link);
 
-  // 오류 감지 시, 오류 처리하는 헬퍼 함수
-  function validate(condition, error_message) {
-    if ( condition ) { throw new Error(error_message); }
-  }
-  // 오디오 객체인지 체크하는 헬퍼 함수
-  function isAudioObject(data) {
-    return data && data.constructor === HTMLAudioElement;
-  }
-
   // 오디오 객체 재생 가능한 시점(oncanplay)이 되면 재생(.play())
   audio.oncanplay = function() {
     audio_time_total.innerHTML = $.getReadableTime(audio.duration);
   };
-
-  // 시크바 프로세스 문서 객체 참조
-  var seekbar_progress   = document.querySelector('.audio-seekbar-progress');
-  var audio_time_current = document.querySelector('.audio-time-current');
-  var audio_time_total   = document.querySelector('.audio-time-total');
 
   // 오디오 객체 재생 중, 시간 업데이트 이벤트 핸들링
   audio.ontimeupdate = function() {
@@ -43,46 +49,19 @@
     audio_time_current.innerHTML = $.getReadableTime(this.currentTime);
   };
 
-  // 외부에서 접근 가능하도록 공개
-  global.audio     = audio;
+  // --------------------------------------------------------------------------
 
-})(window, window.HTMLAudioElement);
-
-
-
-(function(global){
-  'use strict';
-
-  // readableDuration() 함수 정의
-
-})(window);
-
-
-// 오디오 컨트롤 제어
-(function(global, audio){
-  'use strict';
-
-  if (!audio) { return; }
-
-  var btn_play, btn_stop, btn_pause;
-
-  btn_play  = document.querySelector('.audio-control__play');
-  btn_pause = document.querySelector('.audio-control__pause');
-  btn_stop  = document.querySelector('.audio-control__stop');
+  // 시크바 프로세스 문서 객체 참조
+  var seekbar_progress   = document.querySelector('.audio-seekbar-progress');
+  var audio_time_current = document.querySelector('.audio-time-current');
+  var audio_time_total   = document.querySelector('.audio-time-total');
+  var btn_play           = document.querySelector('.audio-control__play');
+  var btn_pause          = document.querySelector('.audio-control__pause');
+  var btn_stop           = document.querySelector('.audio-control__stop');
 
   // Function.prototype.bind 메서드 빌려쓰기 패턴 활용
   btn_play.onclick  = audio.play.bind(audio);
   btn_pause.onclick = audio.pause.bind(audio);
   btn_stop.onclick  = audio.stop.bind(audio);
 
-  // btn_play.onclick = function() {
-    // audio.play();
-  // };
-  // btn_pause.onclick = function() {
-  //   audio.pause();
-  // };
-  // btn_stop.onclick = function() {
-  //   audio.stop();
-  // };
-
-})(window, window.audio);
+})(window, window.HTMLAudioElement);
