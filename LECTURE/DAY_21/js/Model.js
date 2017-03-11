a137293cf0d6723e409cb3b2ac99a2a9c8ec603a
Jee Hoon <yamoo9@naver.com>
2017-03-11 10:49:48 +0900

    Model 생성자 함수 수정(생성된 카운트 기억 변수 추가) / music_playlist 객체 활용 코드 변경

--- LECTURE/DAY_21/js/Model.js
+++ LECTURE/DAY_21/js/Model.js
@@ -1,10 +1,12 @@
 /*! Model.js © yamoo9.net, 2017 */
 
-var Model = (function(global){
+var Model = (function(){
   'use strict';
 
   // 은폐
-  var data_list;
+  var data_list=[];
+  // 생성 카운트
+  var generated_count = 0;
 
   // 생성자 함수
   function _Model(data) {
@@ -13,9 +15,11 @@ var Model = (function(global){
     if ( data && !Array.isArray(data) ) {
       throw new Error('초기 데이터 유형은 배열만 가능합니다.');
     }
+    // 생성된 카운트 기억
+    // this.generated_count = ++generated_count;
+    this.g = generated_count++;
     // 데이터 (배열)
-    data_list = data || [];
-    // this.data = data || [];
+    data_list.push(data || []);
     // 암시적 this 객체 반환
     // return this;
   }
@@ -28,17 +32,17 @@ var Model = (function(global){
   _Model.prototype = {
     'create' : function(new_item) {
       // 모델 데이터에 새로운 아이템을 추가한다.
-      data_list.push(new_item);
+      data_list[this.g].push(new_item);
     },
     'read'   : function(index) {
       // 존재하는 모델 데이터 아이템을 읽어온다. (반환)
       if ( typeof index === 'undefined' ) {
-        return data_list;
+        return data_list[this.g];
       } else {
         if ( index < 0 ) {
           return this.read(this.size() + index);
         } else {
-          return data_list[index];
+          return data_list[this.g][index];
         }
       }
     },
@@ -50,13 +54,13 @@ var Model = (function(global){
     },
     'delete' : function(index) {
       // 존재하는 모델 데이터 아이템을 제거한다.
-      return data_list.splice(index, 1);
+      return data_list[this.g].splice(index, 1);
     },
     'size': function() {
-      return data_list.length;
+      return data_list[this.g].length;
     }
   };
 
   return _Model;
 
-})(window);
+})();
