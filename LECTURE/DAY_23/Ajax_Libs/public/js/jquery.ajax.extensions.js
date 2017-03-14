e53fe31590b378021a277b1ee496a9db560471e1
Jee Hoon <yamoo9@naver.com>
2017-03-14 15:19:28 +0900

    jQuery Ajax 헬퍼 함수 확장

--- LECTURE/DAY_23/Ajax_Libs/public/js/jquery.ajax.extensions.js
+++ LECTURE/DAY_23/Ajax_Libs/public/js/jquery.ajax.extensions.js
@@ -2,6 +2,34 @@
 (function(global, $){
   'use strict';
 
+  // GET, POST
+  // return: jqXHR
+  // $.get(url[, data][, success][, dataType]);
+  // $.post(url[, data][, success][, dataType]);
+  // $.ajax()
 
+  // PUT, DELETE
+  // return jqXHR
+  if (!$.put) {
+    $.put = function(url, data, success, dataType){
+      return $.ajax({
+                 type     : 'PUT',
+                 url      : url,                // [required]
+                 data     : data || null,       // [option]
+                 success  : success || null,    // [option]
+                 dataType : dataType || 'json'  // [option]
+               });
+    };
+  }
+  if (!$.delete) {
+    $.delete = function(url, success, dataType){
+      return $.ajax({
+                 type     : 'DELETE',
+                 url      : url,                // [required]
+                 success  : success || null,    // [option]
+                 dataType : dataType || 'json'  // [option]
+               });
+    };
+  }
 
 })(window, window.jQuery);
