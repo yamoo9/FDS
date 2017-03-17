ae82a9f07b54a785c2979975db1eb2005c8ba080
Jee Hoon <yamoo9@naver.com>
2017-03-17 18:06:41 +0900

    Webpack 설정

---
+++ LECTURE/DAY_25/Webpack/app/app.js
@@ -0,0 +1,14 @@
+// CSS
+// CommonJS
+// require('./style.css');
+
+// ES6
+import style from './style.css';
+import main from './main.sass';
+
+
+// JS
+import Model from './Model';
+import Render from './Render';
+
+let app = {};
