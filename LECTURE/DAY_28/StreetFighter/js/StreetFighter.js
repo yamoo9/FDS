5dda338a783f678590e9b44ae96d28c669af9ee6
Jee Hoon <yamoo9@naver.com>
2017-03-22 14:38:24 +0900

    Stage 배경 이미지 설정 파트 코드 추가

--- LECTURE/DAY_28/StreetFighter/js/StreetFighter.js
+++ LECTURE/DAY_28/StreetFighter/js/StreetFighter.js
@@ -6,10 +6,10 @@
   var esc = 27;
 
   // 사운드 제어
-  var bgm = document.createElement('audio');
+  global.bgm = document.createElement('audio');
   bgm.setAttribute('src', './bgm/Street-Fighter-v5__Ryu-Theme.mp3');
   bgm.oncanplay = function() {
-    this.play();
+    // this.play();
   };
   global.document.addEventListener('keyup', function(e) {
     if ( e.keyCode === esc ) {
@@ -17,3 +17,51 @@
     }
   });
 })(window);
+
+(function(global, Vue, bgm){
+  'use strict';
+
+  var stages = ['ryu', 'factory', 'palace', 'air'];
+
+  function choiceStage(v) {
+    // 전달 인자가 있으면 해당 번호의 스테이지 이름을 반환
+    if ( v && stages[v] ) { return stages[v]; }
+    // 전달 인자가 없으면, 랜덤 스테이지 이름을 반환
+    else { return stages[randomNumber(stages.length)] }
+  }
+
+  function randomNumber(max) {
+    return Math.floor( Math.random() * max );
+  }
+
+  // 스트리트 파이터에 사용되는 상태 데이터 속성
+  // 스테이지
+  // ryu, factory, palace, air
+  var model = {
+    stageClass: null,
+    is_started: false,
+  };
+
+  // 뷰 인스턴스 생성
+  global.vm = new Vue({
+    // 마운트
+    el: '#StreetFighter',
+    // 모델 연결
+    data: model,
+    // 생성된 시점에 음원 재생
+    created: function() {
+      bgm.play();
+    },
+    // 마운트 된 시점에 스테이지 배경 설정
+    mounted: function() {
+      this.choiceStage();
+    },
+    // 인스턴스 메서드
+    methods: {
+      choiceStage: function(v) {
+        this.stageClass = 'stage-' + choiceStage(v);
+      }
+    }
+  });
+
+})(window, window.Vue, window.bgm);
