0e4da8b631cf130b8359c59b18e50f49f181b5cf
Jee Hoon <yamoo9@naver.com>
2017-03-22 14:17:40 +0900

    StreetFighter BGM 추가

--- LECTURE/DAY_28/StreetFighter/js/StreetFighter.js
+++ LECTURE/DAY_28/StreetFighter/js/StreetFighter.js
@@ -1,2 +1,19 @@
 /*! StreetFighter.js © yamoo9.net, 2017 */
 
+(function(global){
+  'use strict';
+  // 멈춤 키
+  var esc = 27;
+
+  // 사운드 제어
+  var bgm = document.createElement('audio');
+  bgm.setAttribute('src', './bgm/Street-Fighter-v5__Ryu-Theme.mp3');
+  bgm.oncanplay = function() {
+    this.play();
+  };
+  global.document.addEventListener('keyup', function(e) {
+    if ( e.keyCode === esc ) {
+      bgm.pause();
+    }
+  });
+})(window);
