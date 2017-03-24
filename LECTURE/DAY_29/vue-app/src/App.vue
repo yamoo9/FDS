1d1a6c60049d6f40513b507fbc87c1d5a1bd2230
Jee Hoon <yamoo9@naver.com>
2017-03-24 18:01:31 +0900

    Vue CLI 사용법 및 컴포넌트 사용 방법

---
+++ LECTURE/DAY_29/vue-app/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <headers></headers>
+  </div>
+</template>
+
+<script>
+import Headers from './components/Headers.vue';
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      msg: 'Hello Vue :-)'
+    }
+  },
+  components: {
+    Headers
+  },
+  computed: {
+    memory() {
+      return 10;
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+  .container
+    width: 800px
+    margin:
+      left: auto
+      right: auto
+</style>
---
+++ LECTURE/DAY_29/vue-app/src/components/Headers.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <h2><a class="app-link" href @click.prevent="callCompAction">{{ msg }}</a></h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return  {
+        msg: 'wow vue file'
+      }
+    },
+    methods: {
+      callCompAction() {
+        window.alert(this.msg);
+      }
+    }
+  }
+</script>
+
+<style lang="sass" scoped>
+  .app-link
+    text-decoration: none
+    color: purple
+</style>
