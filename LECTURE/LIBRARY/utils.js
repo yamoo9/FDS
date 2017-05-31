f2c2f295c5c5f5e95eeeb79b9821de8ed6782db2
Jee Hoon <yamoo9@naver.com>
2017-05-31 18:24:00 +0900

    JSDOC 정리, 수업 때 주석 확인하려면 이전 Commit 확인

--- LECTURE/LIBRARY/utils.js
+++ LECTURE/LIBRARY/utils.js
@@ -19,50 +19,33 @@ function type(data) {
  * @returns {boolean}    - 데이터 일치 검증 결과를 참/거짓으로 반환
  */
 function isType(data, kind) {
-
-  // 데이터 검증 (유효성 검사: validate)
-  // 조건 문: if ~ else
-  // kind 매개변수(전달된 인자)의 유형이 문자열이 아니면... 조건이 참
-  // if (type(kind) !== 'string') {
-  //   throw 'kind 전달 인자는 문자열이 전달되어야 합니다';
-  // }
-
-  // ⬇︎ --------------------------------------------------------------
-
-  // 위 코드 구문을 아래처럼 인라인 코드로 사용할 수 있도록 함수를 정의해보세요.
-
-  // 유효성 검사가 요구됨.
-  // 검사를 통과하지 못하면 오류를 발생하여 코드를 중지
-  // 콘솔에 오류 메시지를 출력하여 사용자에게 정보를 제공.
-  // invalid(kind, 'string');
   validate(kind, '!string', '2번째 전달인자는 문자열이어야 합니다');
-
-  // 유효성 검사를 통과하면 아래 코드를 실행
   return type(data) === kind;
 }
 
-// valid(kind, 'string', 'kind 전달 인자가 문자열이어서는 안됩니다');
-// 인자 1(필수) : 모든 데이터
-// 인자 2(필수) : 데이터 유형 문자열(소문자)
-// 인자 3(선택) : 콘솔에 출력할 오류 메시지
-// 오류 조건: type(kind) === 'string' 비교 값이 참일 경우
-
-// kind 유형 값이 string 이면 오류 메시지 출력
-// 오류 조건: type(kind) === 'string' 비교 값이 참일 경우
-// valid(kind, 'string', 'kind 전달 인자가 문자열이어서는 안됩니다');
-// validate(kind, 'string');
+/**
+ * 전달된 데이터, 데이터 유형을 검증하여 값이 일치할 경우, 오류를 발생시키는 유틸리티 함수
+ *
+ * @param {any} data             - JavaScript 모든 데이터
+ * @param {string} kind          - 오류 검증을 위한 문자열 데이터
+ * @param {string} error_message - 오류 메시지
+ * @returns {string}             - 유효한 경우 출력되는 메시지
+ */
 function valid(data, kind, error_message) {
   if ( type(data) === kind ) {
-    // throw error_message;
     throw error_message || '두 값은 동일하기에 오류입니다.';
   }
   return '오류는 발생하지 않았습니다';
 }
 
-// kind 유형 값이 string 이 아니면, 오류 메시지 출력
-// 오류 조건: type(kind) !== 'string' 비교 값이 참일 경우
-// invalid(kind, 'string', 'kind 전달 인자는 문자열이 전달되어야 합니다');
-// validate(kind, '!string', 'kind 전달 인자는 문자열이 전달되어야 합니다');
+/**
+ * 전달된 데이터, 데이터 유형을 검증하여 값이 불일치할 경우, 오류를 발생시키는 유틸리티 함수
+ *
+ * @param {any} data             - JavaScript 모든 데이터
+ * @param {string} kind          - 오류 검증을 위한 문자열 데이터
+ * @param {string} error_message - 오류 메시지
+ * @returns {string}             - 유효한 경우 출력되는 메시지
+ */
 function invalid(data, kind, error_message) {
   if ( type(data) !== kind ) {
     throw error_message || '두 값이 다르기에 오류입니다.';
@@ -70,10 +53,13 @@ function invalid(data, kind, error_message) {
   return '오류는 발생하지 않았습니다';
 }
 
-// validate() 함수 정의
-// 인자 1(필수) : 모든 데이터
-// 인자 2(필수) : 데이터 유형 문자열(소문자)
-// 인자 3(선택) : 콘솔에 출력할 오류 메시지
+/**
+ * 전달된 데이터, 데이터 유형을 검증하여 조건이 참일 경우, 오류를 발생시키는 유틸리티 함수
+ *
+ * @param {any} data             - JavaScript 모든 데이터
+ * @param {string} kind          - 오류 검증을 위한 문자열 데이터
+ * @param {string} error_message - 오류 메시지
+ */
 function validate(data, kind, error_message) {
   if ( kind.indexOf('!') > -1 ) {
     invalid(data, kind.slice(1), error_message);
